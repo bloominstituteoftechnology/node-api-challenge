b48a33c0180e243e61c1136bb7ef3840f5e98b5a
Luis Hernandez <luishrd@gmail.com>
2019-04-26 08:54:38 -0600

    fixes model error when passing id of non existing project

--- data/helpers/projectModel.js
+++ data/helpers/projectModel.js
@@ -2,45 +2,60 @@ const db = require('../dbConfig.js');
 const mappers = require('./mappers');
 
 module.exports = {
-  get: function(id) {
-    let query = db('projects as p');
+  get,
+  insert,
+  update,
+  remove,
+  getProjectActions,
+};
 
-    if (id) {
-      query.where('p.id', id).first();
+function get(id) {
+  let query = db('projects as p');
 
-      const promises = [query, this.getProjectActions(id)]; // [ projects, actions ]
+  if (id) {
+    query.where('p.id', id).first();
 
-      return Promise.all(promises).then(function(results) {
-        let [project, actions] = results;
+    const promises = [query, this.getProjectActions(id)]; // [ projects, actions ]
+
+    return Promise.all(promises).then(function(results) {
+      let [project, actions] = results;
+
+      if (project) {
         project.actions = actions;
 
         return mappers.projectToBody(project);
-      });
-    }
-
-    return query.then(projects => {
-      return projects.map(project => mappers.projectToBody(project));
+      } else {
+        return null;
+      }
     });
-  },
-  getProjectActions: function(projectId) {
-    return db('actions')
-      .where('project_id', projectId)
-      .then(actions => actions.map(action => mappers.actionToBody(action)));
-  },
-  insert: function(project) {
-    return db('projects')
-      .insert(project)
-      .then(([id]) => this.get(id));
-  },
-  update: function(id, changes) {
-    return db('projects')
-      .where('id', id)
-      .update(changes)
-      .then(count => (count > 0 ? this.get(id) : null));
-  },
-  remove: function(id) {
-    return db('projects')
-      .where('id', id)
-      .del();
-  },
-};
+  }
+
+  return query.then(projects => {
+    return projects.map(project => mappers.projectToBody(project));
+  });
+}
+
+function insert(project) {
+  return db('projects')
+    .insert(project)
+    .then(([id]) => this.get(id));
+}
+
+function update(id, changes) {
+  return db('projects')
+    .where('id', id)
+    .update(changes)
+    .then(count => (count > 0 ? this.get(id) : null));
+}
+
+function remove(id) {
+  return db('projects')
+    .where('id', id)
+    .del();
+}
+
+function getProjectActions(projectId) {
+  return db('actions')
+    .where('project_id', projectId)
+    .then(actions => actions.map(action => mappers.actionToBody(action)));
+}
