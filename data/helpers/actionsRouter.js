4d96b20f4612c5d01c79637790507849f55190cd
Lambda_School_Loaner_112 <lambda_school_loaner_112@Lambda-School-Loaner-112s-MacBook-Pro.local>
2019-09-06 12:01:40 -0500

    All Basic CRUD request working

--- data/helpers/actionsRouter.js
+++ data/helpers/actionsRouter.js
@@ -11,5 +11,51 @@ router.get('/', async (req, res) => {
       res.status(500).json({ message: 'Failed to get actions' });
     }
   }); //endpoint works
+ 
+router.put('/:id', async (req, res) => {
+    const { id } = req.params;
+    const changes = req.body;
+   console.log(changes)
+    try {
+      const action = await Actions.get(id);
   
+      if (action) {
+        const updatedAction = await Actions.update(id, changes);
+        res.json(updatedAction);
+      } else {
+        res.status(404).json({ message: 'Could not find action with given id' });
+      }
+    } catch (err) {
+        console.log(err)
+      res.status(500).json({ message: 'Failed to update action' });
+    }
+  }); //endpoint works 
+
+router.delete('/:id', async (req, res) => {
+    const { id } = req.params;
+  
+    try {
+      const deleted = await Actions.remove(id);
+  
+      if (deleted) {
+        res.json({ removed: deleted });
+      } else {
+        res.status(404).json({ message: 'Could not find action with given id' });
+      }
+    } catch (err) {
+      res.status(500).json({ message: 'Failed to delete action' });
+    }
+  }); //endpoint works
+
+router.post('/', async (req, res) => {
+    const actionData = req.body;
+  
+    try {
+      const action = await Actions.insert(actionData);
+      res.status(201).json(action);
+    } catch (err) {
+      res.status(500).json({ message: 'Failed to create new action' });
+    }
+  }); //endpoint works
+
 module.exports = router;
--- data/helpers/projectsRouter.js
+++ data/helpers/projectsRouter.js
@@ -29,4 +29,33 @@ router.put('/:id', async (req, res) => {
       res.status(500).json({ message: 'Failed to update project' });
     }
   }); //endpoint works
+
+router.delete('/:id', async (req, res) => {
+    const { id } = req.params;
+  
+    try {
+      const deleted = await Projects.remove(id);
+  
+      if (deleted) {
+        res.json({ removed: deleted });
+      } else {
+        res.status(404).json({ message: 'Could not find project with given id' });
+      }
+    } catch (err) {
+      res.status(500).json({ message: 'Failed to delete project' });
+    }
+  });//endpoint works
+
+router.post('/', async (req, res) => {
+    const projectData = req.body;
+  
+    try {
+      const project = await Projects.insert(projectData);
+      res.status(201).json(project);
+    } catch (err) {
+      res.status(500).json({ message: 'Failed to create new project' });
+    }
+  }); //endpoint works
+
+
 module.exports = router;
