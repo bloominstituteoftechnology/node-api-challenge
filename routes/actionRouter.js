7d350ce749de206cb0525850cfb2a2ecbf885b1a
Megan White <mfw247@gmail.com>
2019-09-06 13:01:56 -0700

    added get, put, delete for actions

--- routes/actionRouter.js
+++ routes/actionRouter.js
@@ -2,13 +2,26 @@ const router = require("express").Router();
 const actionDb = require("../data/helpers/actionModel.js")
 const projectDb = require("../data/helpers/projectModel.js")
 
+// get all actions for the project 
+router.get("/",(req,res) => {
+    actionDb
+    .get()
+    .then(response => {
+        res.status(200).json(response)
+    })
+    .catch(error => {
+        console.log(error);
+        res.status(500).json({error: "server error; could not retrieve actions"})
+    })
+})
+
 // get action by id
 router.get("/:id",validateActionId,(req,res) => {
     res.status(200).json(req.response)
 })
 
 // create new action
-router.post("/project/:id", validateProjectId,validateActionId,validateStringLength,(req,res) => {
+router.post("/project/:id", validateProjectId,validateStringLength,(req,res) => {
     const {id} = req.params;
     const {description,notes} = req.body;
     const {body} = req;
@@ -28,6 +41,42 @@ router.post("/project/:id", validateProjectId,validateActionId,validateStringLen
     else {
         res.status(400).json({error: "please add description and notes"})
     }
+})
+
+// update action
+router.put("/:id",validateActionId,validateStringLength,(req,res) => {
+    const {body} = req;
+    const {id} = req.params;
+    const {projectId} = req.resource;
+
+    if (body.description && body.notes) {
+        body.projectId = projectId;
+        actionDb.update(id,body)
+        .then(response => {
+            res.status(200).json(response)
+        })
+        .catch(error => {
+            console.log(error);
+            res.status(500).json({error: "server error; could not update action"})
+        })
+    }
+    else {
+        res.status(400).json({error: "please add valid description and notes"})
+    }
+})
+
+// delete action
+router.delete("/:id",validateActionId,(req,res) => {
+    const {id} = req.params;
+    actionDb
+    .remove(id)
+    .then(response => {
+        res.status(204)
+    })
+    .catch(error => {
+        console.log(error)
+        res.status(500).json({error: "server error; could not delete action"})
+    })
 })
 
 
