94e234bc4d79b8d91b188269d70bb2a79a5f197b
Lambda_School_Loaner_112 <lambda_school_loaner_112@Lambda-School-Loaner-112s-MacBook-Pro.local>
2019-09-06 11:28:47 -0500

    Put requests working

--- data/helpers/projectsRouter.js
+++ data/helpers/projectsRouter.js
@@ -11,6 +11,22 @@ router.get('/', async (req, res) => {
     }
   }); //endpoint works
 
-
-
+router.put('/:id', async (req, res) => {
+    const { id } = req.params;
+    const changes = req.body;
+   console.log(changes)
+    try {
+      const project = await Projects.get(id);
+  
+      if (project) {
+        const updatedProject = await Projects.update(id, changes);
+        res.json(updatedProject);
+      } else {
+        res.status(404).json({ message: 'Could not find project with given id' });
+      }
+    } catch (err) {
+        console.log(err)
+      res.status(500).json({ message: 'Failed to update project' });
+    }
+  }); //endpoint works
 module.exports = router;
