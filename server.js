01f273b30a8fa9fdd10d16a22e17d7f337d13bbc
Megan White <mfw247@gmail.com>
2019-09-06 10:36:37 -0700

    set up action router

--- server.js
+++ server.js
@@ -2,9 +2,11 @@ const express = require("express");
 const server = express();
 
 const projectRouter = require("./routes/projectRouter")
+const actionRouter = require("./routes/actionRouter")
 
 server.use(express.json())
 server.use("/projects",projectRouter)
+server.use("/actions",actionRouter)
 
 server.get("/", (req,res) => {
     res.status(200).json({api: "up"});
