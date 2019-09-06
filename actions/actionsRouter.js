e39fb505f5b497b73b334432be1e00afd78288c8
johnschneider1 <johnlawschneider@gmail.com>
2019-09-06 13:06:16 -0500

    mvp

--- actions/actionsRouter.js
+++ actions/actionsRouter.js
@@ -1,9 +1,86 @@
 const express = require("express");
 
-const db = require("../data/dbConfig.js");
+const db = require("../data/helpers/actionModel");
 
 const router = express.Router();
 
 router.use(express.json());
+
+// Method GET all
+router.get("/", (req, res) => {
+  db.get()
+    .then(actions => {
+      res.status(200).json(actions);
+    })
+    .catch(err => {
+      res.status(500).json({ error: "didn't work" });
+    });
+});
+
+// Method get by id
+router.get("/:id", (req, res) => {
+  const id = req.params.id;
+  db.get(id)
+    .then(proj => {
+      res.status(200).json(proj);
+    })
+    .catch(err => {
+      res.status(500).json({ error: "diddn't work" });
+    });
+});
+
+// Method post
+router.post("/", validateActionsId, (req, res) => {
+  const action = req.body;
+  db.insert(action)
+    .then(actions => {
+      res.status(200).json(actions);
+    })
+    .catch(err => {
+      res.status(500).json({ error: "can't add" });
+    });
+});
+
+// Method delete
+router.delete("/:id", (req, res) => {
+  const id = req.params.id;
+  db.remove(id)
+    .then(actions => {
+      res.status(200).json(actions);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json({ error: "cant delete" });
+    });
+});
+
+// Method update
+router.put("/:id", (req, res) => {
+  const id = req.params.id;
+  const changes = req.body;
+  db.update(id, changes)
+    .then(updated => {
+      res.status(200).json(updated);
+    })
+    .catch(err => {
+      res.status(500).json({ error: "did not get changed" });
+    });
+});
+
+function validateActionsId(req, res, next) {
+  let id = req.params.id;
+
+  db.get(id)
+    .then(actions => {
+      if (actions) {
+        next();
+      } else {
+        res.status(400).json({ message: "user not fouond" });
+      }
+    })
+    .catch(err => {
+      res.status(500).json({ error: "no way" });
+    });
+}
 
 module.exports = router;
--- projects/projectRouter.js
+++ projects/projectRouter.js
@@ -1,9 +1,100 @@
 const express = require("express");
 
-const db = require("../data/dbConfig.js");
+const db = require("../data/helpers/projectModel");
 
 const router = express.Router();
 
 router.use(express.json());
+
+// Method GET all
+router.get("/", (req, res) => {
+  db.get()
+    .then(proj => {
+      res.status(200).json({ proj });
+    })
+    .catch(err => {
+      res.status(500).json({ error: "didn't work" });
+    });
+});
+
+// Method get by id
+router.get("/:id", validateProjectsId, (req, res) => {
+  const id = req.params.id;
+  db.get(id)
+    .then(proj => {
+      res.status(200).json(proj);
+    })
+    .catch(err => {
+      res.status(500).json({ error: "diddn't work" });
+    });
+});
+
+// Method post
+router.post("/", (req, res) => {
+  const proj = req.body;
+  db.insert(proj)
+    .then(project => {
+      res.status(200).json(project);
+    })
+    .catch(err => {
+      res.status(500).json({ error: "can't add" });
+    });
+});
+
+// Method delete
+router.delete("/:id", validateProjectsId, (req, res) => {
+  const id = req.params.id;
+  db.remove(id)
+    .then(project => {
+      res.status(200).json(project);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json({ error: "cant delete" });
+    });
+});
+
+// Method update
+router.put("/:id", validateProjectsId, (req, res) => {
+  const id = req.params.id;
+  const changes = req.body;
+  db.update(id, changes)
+    .then(updated => {
+      res.status(200).json(updated);
+    })
+    .catch(err => {
+      res.status(500).json({ error: "did not get changed" });
+    });
+});
+
+// Method get list of actions
+
+router.get("/:id/actions", validateProjectsId, (req, res) => {
+  const { id } = req.params;
+
+  db.getProjectActions(id)
+    .then(proj => {
+      res.status(200).json(proj);
+    })
+    .catch(err => {
+      res.status(500).json({ error: "diddn't work" });
+    });
+});
+
+function validateProjectsId(req, res, next) {
+  let id = req.params.id;
+
+  db.get(id)
+    .then(proj => {
+      if (proj) {
+        next();
+      } else {
+        res.status(400).json({ message: "user not fouond" });
+      }
+    })
+    .catch(err => {
+      res.status(500).json({ error: "no way" });
+    });
+}
 
 module.exports = router;
