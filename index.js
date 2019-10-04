9ba3bacf8c442f92d761717514a5286fafc67b5d
Mimi Hoang <miminhoang1@gmail.com>
2019-10-04 11:30:29 -0400

    Routers and server created

--- index.js
+++ index.js
@@ -12,3 +12,8 @@ I need this code, just don't know where, perhaps should make some middleware, do
 
 Go code!
 */
+const server = require("./server.js");
+
+const port = 9000;
+
+server.listen(port, () => console.log(`\n Check out my API ON  ${port} **\n`));
