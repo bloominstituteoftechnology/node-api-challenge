3296ff07f5c0e7f2c5828da1328b0cd7ff1b3d2f
Mike Nguyen <mpvnguyen1996@gmail.com>
2019-11-01 12:38:04 -0400

    mvp complete

--- data/routers/projectsRouter.js
+++ data/routers/projectsRouter.js
@@ -29,6 +29,56 @@ server.get("/:id", validateId, (req, res) => {
     })
 })
 
+//GET list of Actions
+server.get("/:id/actions", validateId, (req, res) => {
+    const id  = req.params.id;
+    db.getProjectActions(id)
+      .then(project => {
+        res.status(200).json(project);
+      })
+      .catch(err => {
+        res.status(500).json({ error: "GET /:id/actions error" });
+      });
+  });
+
+//POST
+server.post("/", (req, res) => {
+    const body = req.body;
+    db.insert(body)
+    .then(project => {
+        res.status(200).json(project);
+    })
+    .catch(error => {
+        res.status(500).json({ error: "POST / error" })
+    })
+})
+
+//DELETE
+server.delete("/:id", validateId, (req, res) => {
+    const id = req.params.id;
+    db.remove(id)
+      .then(project => {
+        res.status(200).json(project);
+      })
+      .catch(err => {
+        console.log(err);
+        res.status(500).json({ error: "cant delete" });
+      });
+  });
+
+//PUT
+server.put("/:id", validateId, (req, res) => {
+    const id = req.params.id;
+    const body = req.body;
+    db.update(id, body)
+      .then(response => {
+        res.status(200).json(response);
+      })
+      .catch(error => {
+        res.status(500).json({ error: "did not get changed" });
+      });
+  });
+  
 
 
 //middleware
