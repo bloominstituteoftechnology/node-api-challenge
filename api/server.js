bce75c09cf2b8e3d805f4cb458fd89fb4ffb1979
Vyraal1 <kevin.afable@gmail.com>
2019-11-01 11:35:36 -0400

    server setup

---
+++ api/server.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const helmet = require("helmet");
+
+// const actionsRouter = require("./actionsRouter");
+// const projectsRouter = require("./projectsRouter");
+
+const server = express();
+
+const logger = prefix => (req, res, next) => {
+  const { url, method } = req;
+  const timestamp = new Date().toISOString();
+  console.log(`${prefix} : [${timestamp}] ${method} ${url}`);
+  next();
+};
+
+server.use(helmet());
+server.use(logger("Logger"));
+server.use(express.json());
+
+// Routes
+
+server.use(`/api/actions`, actionsRouter);
+server.use(`/api/projects`, projectsRouter);
+
+server.get(`/`, (req, res) => {
+  res.status(200).json({ message: `I'm in the mainframe.` });
+});
+
+module.exports = server;
