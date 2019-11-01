c85ea1b3dc35541ba7ccc78165c8cc4b31e63d33
Mike Nguyen <mpvnguyen1996@gmail.com>
2019-11-01 12:24:00 -0400

    GET requests functional on projectsRouter + middleware to validate ID

--- data/routers/projectsRouter.js
+++ data/routers/projectsRouter.js
@@ -1,9 +1,51 @@
 const express = require("express");
 
-const db = require("../dbConfig");
+const db = require("../helpers/projectModel.js");
 
 const server = express.Router();
 
 server.use(express.json());
+
+//GET
+server.get("/", (req, res) => {
+    db.get()
+    .then(projects => {
+        res.status(200).json({projects})
+    })
+    .catch(error => {
+        res.status(500).json({ error:"GET / error" })
+    })
+})
+
+//GET by ID
+server.get("/:id", validateId, (req, res) => {
+    const id = req.params.id;
+    db.get(id)
+    .then(project => {
+        res.status(200).json(project)
+    })
+    .catch(error => {
+        res.status(500).json({ error: "GET /:id error" })
+    })
+})
+
+
+
+//middleware
+//validateID
+function validateId(req, res, next) {
+    let id = req.params.id;
+    db.get(id)
+      .then(actions => {
+        if (actions) {
+          next();
+        } else {
+          res.status(400).json({ message: "id is invalid" });
+        }
+      })
+      .catch(err => {
+        res.status(500).json({ error: "YOU SHALL NOT PASS!!" });
+      });
+  }
 
 module.exports = server;
