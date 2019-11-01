8801d6b2b04f1c76ae31133918eb861d00932873
Chazaray Robinson <chaarayrobinson@Chazarays-MacBook-Pro.local>
2019-11-01 11:47:11 -0400

    server created

--- index.js
+++ index.js
@@ -12,3 +12,8 @@ I need this code, just don't know where, perhaps should make some middleware, do
 
 Go code!
 */
+const server = require('./server.js')
+const port = 5500
+server.listen(port, () => {
+    console.log('Server is running', port)
+})
