bc87c5088c4c9c235dd146a934afb24c28f3a632
benjamin-t-wilson <benjiimanwillson@gmail.com>
2019-11-09 11:15:34 -0700

    finished projects

--- routes/projectRouter.js
+++ routes/projectRouter.js
@@ -60,6 +60,17 @@ router.delete("/:id", validateID, (req, res) => {
     });
 });
 
+router.get("/:id/actions", validateID, (req, res) => {
+  const id = req.params.id;
+
+  db.getProjectActions(id)
+    .then(actions => {
+      res.status(200).json(actions);
+    })
+    .catch(err => {
+      res.status(500).json({ message: "Error retrieving actions", err });
+    });
+});
 // *** MIDDLEWARE ***
 
 function validateID(req, res, next) {
