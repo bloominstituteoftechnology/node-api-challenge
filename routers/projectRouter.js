aa57c20439c0dadfce38c8efc639644f0c1730f3
Brandon Davis <49970067+Bdavis90@users.noreply.github.com>
2019-11-08 23:25:25 -0500

    created get request for project endpoint

--- routers/projectRouter.js
+++ routers/projectRouter.js
@@ -3,4 +3,44 @@ const express = require("express");
 const Project = require("../data/helpers/projectModel");
 
 const router = express.Router();
+
+router.get("/", (req, res) => {
+  Project.get()
+    .then(project => res.status(200).json(project))
+    .catch(err => res.status(500).json({ message: "Could not get project" }));
+});
+
+router.get("/:id", validateProjectId, (req, res) => {
+  res.status(200).json(req.project);
+});
+
+router.post("/", (req, res) => {
+  const project = req.body;
+  // if (!req.body.description) {
+  //   return res.status(400).json({ message: "Must add a description" });
+  // } else if (!req.body.notes) {
+  //   return res.status(400).json({ message: "Must add a note" });
+  // } else
+  Project.insert(project)
+    .then(project => res.status(201).json(project))
+    .catch(err =>
+      res.status(500).json({ message: "Could not create new post." })
+    );
+});
+
+function validateProjectId(req, res, next) {
+  const id = req.params.id;
+  Project.get(id)
+    .then(project => {
+      if (project) {
+        req.project = project;
+        next();
+      } else {
+        res.status(404).json({ message: "Project could not be found." });
+      }
+    })
+    .catch(err => {
+      res.status(500).json({ message: "This id does not exsist" });
+    });
+}
 module.exports = router;
