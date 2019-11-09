9ac736e7c152bb2b1e05bbe812c8fe8edcc6053b
Brandon Davis <49970067+Bdavis90@users.noreply.github.com>
2019-11-08 21:46:03 -0500

    created get request for action /:id endpoint

--- routers/actionRouter.js
+++ routers/actionRouter.js
@@ -14,4 +14,15 @@ router.get("/", (req, res) => {
     });
 });
 
+router.get("/:id", (req, res) => {
+  const id = req.params.id;
+  Action.get(id)
+    .then(action => {
+      res.status(200).json(action);
+    })
+    .catch(err => {
+      res.status(500).json({ message: "There was an error!" });
+    });
+});
+
 module.exports = router;
