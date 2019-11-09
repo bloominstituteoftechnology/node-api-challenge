7d98c6a72cc15b31e92e9d70e508f854c725dea5
Brandon Davis <49970067+Bdavis90@users.noreply.github.com>
2019-11-08 21:43:33 -0500

    created get request for action / endpoint

--- routers/actionRouter.js
+++ routers/actionRouter.js
@@ -3,4 +3,15 @@ const express = require("express");
 const Action = require("../data/helpers/actionModel");
 
 const router = express.Router();
+
+router.get("/", (req, res) => {
+  Action.get()
+    .then(action => {
+      res.status(200).json(action);
+    })
+    .catch(err => {
+      res.status(500).json({ message: "There was an error!" });
+    });
+});
+
 module.exports = router;
