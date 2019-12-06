7977f1ce7489688781880ea6a1a7b45d989331cf
Ale <alearanibar@gmail.com>
2019-12-06 08:39:01 -0800

    start

--- index.js
+++ index.js
@@ -12,3 +12,11 @@ I need this code, just don't know where, perhaps should make some middleware, do
 
 Go code!
 */
+require('dotenv').config()
+
+const server = require('./server.js')
+const port = process.env.PORT
+
+server.listen(port, () => {
+    console.log(`\n*** Server Running on http://localhost:${port} `)
+})
---
+++ server.js
@@ -0,0 +1,8 @@
+const express = require('express')
+const helmet = require('helmet')
+
+
+
+const server = express()
+
+const 
