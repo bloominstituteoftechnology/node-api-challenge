0518b855aa13565a2c804b405351f01a7141b8f1
CJ Lucido <c.jose.lucido@gmail.com>
2019-12-06 08:35:08 -0800

    added dotenv as dependency, wrote index file

--- index.js
+++ index.js
@@ -1,14 +1,10 @@
-/*
-play this: https://www.youtube.com/watch?v=d-diB65scQU
+require("dotenv").config()
 
-Sing along:
 
-here's a little code I wrote, please read the README word for word, don't worry, you got this
-in every task there may be trouble, but if you worry you make it double, don't worry, you got this
-ain't got no sense of what is REST? just concentrate on learning Express, don't worry, you got this
-your file is getting way too big, bring a Router and make it thin, don't worry, be crafty
-there is no data on that route, just write some code, you'll sort it out… don't worry, just API…
-I need this code, just don't know where, perhaps should make some middleware, don't worry, just API
+const server = require('./server')
 
-Go code!
-*/
+const port = process.env.PORT || 4500
+
+server.listen(port, ()=>{
+    console.log(`\n Server listening on http://localhost:${port}`)
+})
