e0685079706de5efdc61aa40fda7ef5eac35f595
CJ Lucido <c.jose.lucido@gmail.com>
2019-12-06 09:01:44 -0800

    added get ops for all actions and projects, added post op for projects

--- actions/actionsRouter.js
+++ actions/actionsRouter.js
@@ -12,6 +12,42 @@ const router = express.Router()
 
 //CRUD OPS
 
+//GET
+
+
+router.get("/", (req, res)=>{
+    Actions.get()
+    .then(projects => {
+        res.status(200).json(projects)
+    })
+    .catch(err => {
+        res.status(500).json({error: "Error req projects on GET root"})
+    })
+})
+
+// //POST
+
+// router.post('/', (req, res)=>{
+
+
+
+// })
+
+
+// //PUT
+
+// router.put('/', (req, res)=>{
+
+
+    
+// })
+
+// //DEL
+// router.delete('/', (req, res)=>{
+
+
+    
+// })
 
 //MIDDLEWARE
 
--- projects/projectsRouter.js
+++ projects/projectsRouter.js
@@ -12,6 +12,50 @@ const router = express.Router()
 
 //CRUD OPS
 
+//GET
+
+router.get("/", (req, res)=>{
+    Projectos.get()
+    .then(projects => {
+        res.status(200).json(projects)
+    })
+    .catch(err => {
+        res.status(500).json({error: "Error req projects on GET root"})
+    })
+})
+
+
+//POST
+
+router.post('/', (req, res)=>{
+
+    Projectos.insert(req.body)
+    .then(newResource => {
+            res.status(200).json(newResource)
+    })
+    .catch(err => {
+        res.status(500).json({error: "Failed on projects POST, check request body"})
+    }
+    )
+
+})
+
+
+// //PUT
+
+// router.put('/', (req, res)=>{
+
+
+    
+// })
+
+// //DEL
+// router.delete('/', (req, res)=>{
+
+
+    
+// })
+
 
 //MIDDLEWARE
 
