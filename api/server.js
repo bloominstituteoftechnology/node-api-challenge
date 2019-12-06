e777e6aaa906bceb9e91d0f02ccf97f20a0fe352
Rasha Moumneh <rasha@fastmail.com>
2019-12-06 11:26:02 -0500

    Set up index.js and server.js

---
+++ api/server.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const helmet = require("helmet");
+
+const server = express();
+
+server.use(helmet());
+server.use(express.json());
+
+server.get("/", (req, res) => {
+  res.send("Server up and running");
+});
+
+module.exports = server;
--- index.js
+++ index.js
@@ -1,14 +1,7 @@
-/*
-play this: https://www.youtube.com/watch?v=d-diB65scQU
+const server = require("./api/server");
 
-Sing along:
+const port = process.env.PORT || 4000;
 
-here's a little code I wrote, please read the README word for word, don't worry, you got this
-in every task there may be trouble, but if you worry you make it double, don't worry, you got this
-ain't got no sense of what is REST? just concentrate on learning Express, don't worry, you got this
-your file is getting way too big, bring a Router and make it thin, don't worry, be crafty
-there is no data on that route, just write some code, you'll sort it out… don't worry, just API…
-I need this code, just don't know where, perhaps should make some middleware, don't worry, just API
-
-Go code!
-*/
+server.listen(port, () => {
+  console.log(`\n*** Server Running on http://localhost:${port} ***\n`);
+});
