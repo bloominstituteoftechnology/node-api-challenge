b135f55541f0bcbeebf3b21ccbbbbda867fd19e3
Ale <alearanibar@gmail.com>
2019-12-06 08:57:29 -0800

    server

--- server.js
+++ server.js
@@ -5,4 +5,16 @@ const helmet = require('helmet')
 
 const server = express()
 
-const 
+const aRouter = require('./routes/aRouter')
+const pRouter = require('./routes/pRouter')
+
+server.use(express.json())
+
+server.use('/actions', aRouter)
+server,use('/projects', pRouter)
+
+server.get('/', (req, res) => {
+    res.send(`<h1>Welcome to Node Sprint</h1>`)
+})
+
+module.exports = server
