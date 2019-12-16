afecda5270e7baf31acd45cd7b07edc850c4b203
spettigrew <sara.pettigrew5@gmail.com>
2019-12-16 16:09:26 -0700

    Req names changed

--- middleware/validateProject.js
+++ middleware/validateProject.js
@@ -1,9 +1,9 @@
 async function validateProject(req, res, next) {
-    if (!req.body.name) {
+    if (!req.body.project_id) {
         return res.status(400).json({ message: "missing project id number" })
     }
 
-    if (!req.body) {
+    if (!req.body.text) {
         return res.status(400).json({ message: "missing required description" })
     }
     next()
