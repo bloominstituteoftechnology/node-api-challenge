cc4700d635cca6baa2172ec8396e629169b5ef83
spettigrew <sara.pettigrew5@gmail.com>
2019-12-16 16:08:51 -0700

    Req names changed

--- middleware/validateAction.js
+++ middleware/validateAction.js
@@ -1,13 +1,13 @@
 async function validateAction(req, res, next) {
-    if (!req.body.id) {
+    if (!req.body.name) {
         return res.status(400).json({ message: "missing action name" })
     }
 
-    if (!req.body.name) {
+    if (!req.body.text) {
         return res.status(400).json({ message: "missing required description" })
     }
 
-    if (!req.body.name) {
+    if (!req.body.notes) {
         return res.status(400).json({ message: "missing required additional notes" })
     }
     next(err)
