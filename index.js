c27e9606b1eb40a6e9e6bce9178b813f1e87ffe2
Cole Gonzales <gcolegonzales003@gmail.com>
2019-12-15 20:05:46 -0600

    MVP completed

--- index.js
+++ index.js
@@ -1,14 +1,20 @@
-/*
-play this: https://www.youtube.com/watch?v=d-diB65scQU
+const projectsRouter = require('./routers/projectsRouter')
+const actionsRouter = require('./routers/actionsRouter')
 
-Sing along:
+const express = require('express')
+const app = express()
 
-here's a little code I wrote, please read the README word for word, don't worry, you got this
-in every task there may be trouble, but if you worry you make it double, don't worry, you got this
-ain't got no sense of what is REST? just concentrate on learning Express, don't worry, you got this
-your file is getting way too big, bring a Router and make it thin, don't worry, be crafty
-there is no data on that route, just write some code, you'll sort it out… don't worry, just API…
-I need this code, just don't know where, perhaps should make some middleware, don't worry, just API
+app.use(express.json())
+app.use('/api/projects', projectsRouter)
+app.use('/api/projects/:project_id/actions', actionsRouter)
 
-Go code!
-*/
+app.get('/', (req, res) => {
+    res.json({ message: "Welcome to my API!" })
+})
+
+// const port = process.env.PORT || 8000
+// const host = process.env.HOST || "localhost:"
+
+app.listen(8000, () => {
+    console.log(`Server is listening on PORT 8000...`)
+})
