4a58dd4e474cbad2751eac13975c1037d039c802
spettigrew <sara.pettigrew5@gmail.com>
2019-12-16 17:15:50 -0700

    Name request error

--- middleware/validateProject.js
+++ middleware/validateProject.js
@@ -1,9 +1,12 @@
 async function validateProject(req, res, next) {
-    if (!req.body.project_id) {
-        return res.status(400).json({ message: "missing project id number" })
+    // if (!req.params.id) {
+    //     return res.status(400).json({ message: "missing project id number" })
+    // }
+    if (!req.body.name) {
+        return res.status(400).json({ message: "missing required name" })
     }
 
-    if (!req.body.text) {
+    if (!req.body.description) {
         return res.status(400).json({ message: "missing required description" })
     }
     next()
