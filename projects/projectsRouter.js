331391ecc0e7ad2de3ee8ca5f92a8cabc78291fb
Chelsea Wetzel <chelseagilmore14@gmail.com>
2020-01-17 11:57:21 -0500

    get all projects

--- projects/projectsRouter.js
+++ projects/projectsRouter.js
@@ -1,9 +1,24 @@
 const express = require("express");
 
+const projectDb = require("../data/helpers/projectModel.js")
+
 const router = express.Router();
 
+// router.get("/", (req, res) => {
+//     res.send("projectsRouter working!")
+// });
+
 router.get("/", (req, res) => {
-    res.send("projectsRouter working!")
+    projectDb.get()
+    .then(projects => {
+        res.status(200).json(projects);
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json({
+            error: "The projects information could not be found"
+        });
+    });
 });
 
 module.exports = router;
