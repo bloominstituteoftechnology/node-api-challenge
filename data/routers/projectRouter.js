d1398104f6dc2e92c053fbbec07e63ec230f7b4c
Aasa-Christian <aasachristian@yahoo.com>
2020-01-17 12:01:31 -0500

    created get request for all projects

--- data/routers/projectRouter.js
+++ data/routers/projectRouter.js
@@ -2,6 +2,23 @@ const express = require('express');
 
 const router = express.Router();
 
+const projectDB = require('../helpers/projectModel')
+
 router.use(express.json());
+
+
+router.get('/projects', (req, res) => {
+    projectDB.get()
+    .then(hubs => {
+      res.status(200).json(hubs);
+    })
+    .catch(error => {
+      // log error to database
+      console.log(error);
+      res.status(500).json({
+        error: "The project information could not be retrieved."
+      });
+    });
+  });
 
 module.exports = router;
