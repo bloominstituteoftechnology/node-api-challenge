6987195f64c43e3469f71d8e9cdbbf040c0df395
Chelsea Wetzel <chelseagilmore14@gmail.com>
2020-01-17 13:58:46 -0500

    delete an action

--- actions/actionsRouter.js
+++ actions/actionsRouter.js
@@ -2,6 +2,8 @@ const express = require("express");
 
 const actionsDb = require("../data/helpers/actionModel.js");
 
+const projectDb = require("../data/helpers/projectModel.js");
+
 const router = express.Router();
 
 // router.get("/", (req, res) => {
@@ -47,6 +49,58 @@ router.get("/:id", (req, res) => {
       });
     });
 });
+
+// // create a new action
+// router.post("/:id/actions", (req, res) => {
+//     const body = req.body;
+//     actionsDb.insert(body);
+//     const id = req.params.id;
+
+//     projectDb.get(id)
+//     .then(newAction => {
+//         if (!id) {
+//             res.status(404).json({
+//                 message: "The project with the specific ID does not exist"
+//             })
+//         } else if (!newAction.description && !newAction.notes) {
+//             res.status(400).json({
+//                 errorMessage: "Please provide a description and notes for your new action"
+//             })
+//         } else {
+//             res.status(201).json({ newAction })
+//         }
+//     })
+//     .catch(err => {
+//         console.log(err);
+//         res.status(500).json({
+//             errorMessage: "There was an error while adding the action to the database"
+//         });
+//     });
+// });
+
+// deletes an action
+router.delete("/:id", (req, res) => {
+    const id = req.params.id;
+  
+    actionsDb
+      .remove(id)
+      .then(deletedA => {
+        if (!id) {
+          res.status(404).json({
+            message: "The action with the specific ID does not exist"
+          });
+        } else {
+          res.status(200).json({ deletedA });
+        }
+      })
+      .catch(err => {
+        console.log(err);
+        res.status(500).json({
+          error: "The action could not be removed"
+        });
+      });
+  });
+
 
 
 module.exports = router;
