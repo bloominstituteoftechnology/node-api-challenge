e73c5bf1c41838808fe8a700e92e75edd3152f05
Aasa-Christian <aasachristian@yahoo.com>
2020-01-17 12:24:07 -0500

    created post put and delete routes

--- data/routers/projectRouter.js
+++ data/routers/projectRouter.js
@@ -7,6 +7,26 @@ const projectDB = require('../helpers/projectModel')
 router.use(express.json());
 
 
+router.post('/projects/', (req, res) => {
+    projectDB.insert(req.body)
+    .then(hubs => {
+        if(!req.body.name || req.body.name.length<1){
+            res.status(404).json({message: "project name required"})
+        } else{
+          res.status(200).json(hubs);  
+        }
+      
+    })
+    .catch(error => {
+    
+      console.log(error);
+      res.status(500).json({
+        error: "The project information could not be added."
+      });
+    });
+  });
+
+
 router.get('/projects/:id', (req, res) => {
     projectDB.get(req.params.id)
     .then(hubs => {
@@ -18,13 +38,50 @@ router.get('/projects/:id', (req, res) => {
       
     })
     .catch(error => {
-      // log error to database
+    
       console.log(error);
       res.status(500).json({
         error: "The project information could not be retrieved."
       });
     });
   });
+
+  router.put('/projects/:id', (req, res) => {
+    projectDB.update(req.params.id, req.body)
+    .then(hubs => {
+        res.status(200).json(hubs);  
+      
+    })
+    .catch(error => {
+      // log error to database
+      console.log(error);
+      res.status(500).json({
+        error: "The project information could not be updated."
+      });
+    });
+  });
+
+  router.delete('/projects/:id', (req, res) => {
+    projectDB.remove(req.params.id)
+    .then(hubs => {
+        if(!hubs){
+            res.status(404).json({message: "Id cannot be found"})
+        } else{
+          res.status(200).json(hubs);  
+        }
+      
+    })
+    .catch(error => {
+      console.log(error);
+      res.status(500).json({
+        error: "The project information could not be removed."
+      });
+    });
+  });
+
+
+ 
+
 
 
 
