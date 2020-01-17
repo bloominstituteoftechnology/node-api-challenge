0c3e1c392d2fd7a35bde60521fe6588dbfe65f7a
Luis Hernandez <luishrd@gmail.com>
2020-01-16 23:30:32 -0700

    adds production env to knexfile

--- knexfile.js
+++ knexfile.js
@@ -1,20 +1,38 @@
 module.exports = {
-  development: {
-    client: "sqlite3",
-    useNullAsDefault: true,
-    connection: {
-      filename: "./data/lambda.db3"
+    development: {
+        client: "sqlite3",
+        useNullAsDefault: true,
+        connection: {
+            filename: "./data/lambda.db3",
+        },
+        pool: {
+            afterCreate: (conn, done) => {
+                conn.run("PRAGMA foreign_keys = ON", done);
+            },
+        },
+        migrations: {
+            directory: "./data/migrations",
+        },
+        seeds: {
+            directory: "./data/seeds",
+        },
     },
-    pool: {
-      afterCreate: (conn, done) => {
-        conn.run("PRAGMA foreign_keys = ON", done);
-      }
+    production: {
+        client: "sqlite3",
+        useNullAsDefault: true,
+        connection: {
+            filename: "./data/lambda.db3",
+        },
+        pool: {
+            afterCreate: (conn, done) => {
+                conn.run("PRAGMA foreign_keys = ON", done);
+            },
+        },
+        migrations: {
+            directory: "./data/migrations",
+        },
+        seeds: {
+            directory: "./data/seeds",
+        },
     },
-    migrations: {
-      directory: "./data/migrations"
-    },
-    seeds: {
-      directory: "./data/seeds"
-    }
-  }
 };
