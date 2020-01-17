320504167b21ef04e6f596c4e95b718d0fd1667b
Luis Hernandez <luishrd@gmail.com>
2020-01-16 23:32:25 -0700

    formats to 4 spaces

--- data/migrations/20180405083150_projects.js
+++ data/migrations/20180405083150_projects.js
@@ -1,13 +1,13 @@
-exports.up = function(knex, Promise) {
-  return knex.schema.createTable('projects', function(projects) {
-    projects.increments();
+exports.up = function(knex) {
+    return knex.schema.createTable("projects", function(projects) {
+        projects.increments();
 
-    projects.string('name', 128).notNullable();
-    projects.text('description').notNullable();
-    projects.boolean('completed').defaultTo(false);
-  });
+        projects.string("name", 128).notNullable();
+        projects.text("description").notNullable();
+        projects.boolean("completed").defaultTo(false);
+    });
 };
 
-exports.down = function(knex, Promise) {
-  return knex.schema.dropTableIfExists('projects');
+exports.down = function(knex) {
+    return knex.schema.dropTableIfExists("projects");
 };
