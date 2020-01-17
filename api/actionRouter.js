abe94a4a815ea1b9836409ecf4d05d7c66fa9fbb
jarrodskahill <jarrodskahill@gmail.com>
2020-01-17 09:54:12 -0800

    added my middleware to project router

--- api/actionRouter.js
+++ api/actionRouter.js
@@ -18,10 +18,42 @@ const validateId = (req, res, next) => {
 }
 
 router.get('/:id', validateId, (req, res) => {
-    const {id} = req.params
+    const { id } = req.params
     Db.get(id)
     .then(info => res.status(200).json(info))
     .catch(error => res.status(500).json({message: 'Nothing was Retrevied'}))
 })
+
+router.delete('/:id', validateId, (req, res) => {
+    const { id } = req.params
+    Db
+        .remove(id)
+        .then(() => res.status(200).json(`${req.action.description} was deleted`))
+        .catch(err => res.status(500).json({ message: 'Server was unable to delete action', err }))
+})
+
+const validateAction = (req, res, next) => {
+    const { description } = req.body
+
+    if (!req.body) {
+        res.status(400).json({ message: 'Please add information' })
+    } else if (!description) {
+        res.status(400).json({ message: 'Missing description' })
+    } else if (description.length > 128){
+        res.status(400).json({ message: 'Description too long'})
+    } else {
+        next()
+    }
+}
+
+router.put('/:id', validateId, validateAction, (req, res) => {
+    const { id } = req.params
+    const changes = req.body
+        Db
+            .update(id, changes)
+            .then(() => res.status(200).json(`${req.action.description} was updated`))
+            .catch(err => res.status(500).json({ message: 'Server was unable to update', err }))
+})
+
 
 module.exports = router
---
+++ api/projectRouter.js
@@ -0,0 +1,55 @@
+const express = require('express')
+const projectDb = require('../data/helpers/projectModel')
+const actionDb = require('../data/helpers/actionModel')
+
+
+const validateId = (req, res, next) => {
+    const { id } = req.params
+        projectDb
+            .get(id)
+            .then(project => {
+                if (project) {
+                    req.project = project
+                    next()
+                } else {
+                    res.status(400).json({ message: 'Project does not exist' })
+                }
+            })
+            .catch(err => res.status(500).json({ message: 'Server unable to retrieve Project', error: err }))
+    }
+const validateProject = (req, res, next) => {
+    const { name, description } = req.body
+
+        if (!req.body) {
+            res.status(400).json({ message: 'Please add information' })
+        } else if (!name) {
+            res.status(400).json({ message: 'Missing name' })
+        } else if (!description){
+            res.status(400).json({ message: 'Missing description' })
+        } else {
+            next()
+        }
+    }
+
+const validateAction = (req, res, next) => {
+    const { description, notes } = req.body
+
+        if (!req.body) {
+            res.status(400).json({ message: 'Please add information' })
+        } else if (!description) {
+            res.status(400).json({ message: 'Missing description' })
+        } else if (description.length > 128){
+            res.status(400).json({ message: 'Description too long'})
+        } else if (!notes){
+            res.status(400).json({ message: 'Missing notes' })
+        } else {
+            next()
+        }
+    }
+
+router.get('/', (req, res) => {
+    projectDb
+        .get()
+        .then(projects => res.status(200).json(projects))
+        .catch(err => res.status(500).json({ message: 'Server was unable to retrieve projects', error: err }))
+})
