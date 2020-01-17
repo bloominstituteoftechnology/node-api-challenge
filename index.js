8c97c0a89c77581daf549502d708e57594a372f0
Luis Hernandez <luishrd@gmail.com>
2020-01-16 23:30:20 -0700

    updates song

--- index.js
+++ index.js
@@ -7,8 +7,8 @@ here's a little code I wrote, please read the README word for word, don't worry,
 in every task there may be trouble, but if you worry you make it double, don't worry, you got this
 ain't got no sense of what is REST? just concentrate on learning Express, don't worry, you got this
 your file is getting way too big, bring a Router and make it thin, don't worry, be crafty
-there is no data on that route, just write some code, you'll sort it out… don't worry, just API…
-I need this code, just don't know where, perhaps should make some middleware, don't worry, just API
+there is no data on that route, just write some code, you'll sort it out… don't worry, just hack it…
+I need this code, but don't know where, perhaps should make some middleware, don't worry, just hack it
 
 Go code!
 */
