5f946d949688a95d0cfb2e5d4a2793f874ebd4d6
Chelsea Wetzel <chelseagilmore14@gmail.com>
2020-01-17 12:14:45 -0500

    get action by id

--- actions/actionsRouter.js
+++ actions/actionsRouter.js
@@ -9,15 +9,38 @@ const router = express.Router();
 // });
 
 router.get("/", (req, res) => {
-    actionsDb.get()
+  actionsDb
+    .get()
     .then(projects => {
-        res.status(200).json(projects);
+      res.status(200).json(projects);
     })
     .catch(err => {
-        console.log(err);
-        res.status(500).json({
-            error: "The projects information could not be found"
+      console.log(err);
+      res.status(500).json({
+        error: "The actions information could not be found"
+      });
+    });
+});
+
+router.get("/:id", (req, res) => {
+  const id = req.params.id;
+
+  actionsDb
+    .get(id)
+    .then(specific => {
+      if (id) {
+        res.status(200).json(specific);
+      } else {
+        res.status(404).json({
+          error: "No action with that ID"
         });
+      }
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json({
+        error: "The actions information could not be found"
+      });
     });
 });
 
