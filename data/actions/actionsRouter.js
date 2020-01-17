45be38b6d94535b92f2055cb4b261e26e958ae97
cappers86 <52895127+cappers86@users.noreply.github.com>
2020-01-17 15:06:24 +0000

    actions get by id working

--- data/actions/actionsRouter.js
+++ data/actions/actionsRouter.js
@@ -1,4 +1,29 @@
 const express = require('express')
+
+
+const Actions = require('../helpers/actionModel');
+
 const router = express.Router()
 
+router.get('/:id', (req, res) => {
+    const id = req.params.id;
+    Actions.get(id)
+        .then (actions => {
+            if (!id) {
+                res.status(404).json({ message: "ID does not exist." })
+            } else {
+                res.status(200).json(actions)
+            }
+        })
+
+        .catch(error => {
+            console.log(error);
+            res.status(500).json({
+                error: "this information could not be found"
+            })
+        })
+})
+
 module.exports = router;
+
+
