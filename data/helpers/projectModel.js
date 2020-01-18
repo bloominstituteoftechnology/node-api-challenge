e129532cc55ff66e8166ca8d2e76b5865f2a7e79
John Afolabi <johnafolabi.da@gmail.com>
2020-01-18 12:48:35 +0100

    Fix bug to return project by id and actions

--- data/helpers/projectModel.js
+++ data/helpers/projectModel.js
@@ -15,7 +15,7 @@ function get(id) {
     if (id) {
         query.where("p.id", id).first();
 
-        const promises = [query, this.getProjectActions(id)]; // [ projects, actions ]
+        const promises = [query, getProjectActions(id)]; // [ projects, actions ]
 
         return Promise.all(promises).then(function(results) {
             let [project, actions] = results;
