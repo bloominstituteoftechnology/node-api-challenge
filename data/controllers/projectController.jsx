7e27894199e268cb02dd51f3297a5ef672203211
Brett Karr <b.karr@polyclip.us>
2020-01-25 11:42:15 -0600

    validatePost works

--- data/controllers/projectController.jsx
+++ data/controllers/projectController.jsx
@@ -1,19 +1,37 @@
 const projectController = require("../helpers/projectModel");
 console.log("\nprojectController.jsx is running....\n");
 
-exports.getStuff = (req, res, next) => {
-  console.log("getStuff");
+exports.getProjects = (req, res, next) => {
+  console.log("getProjects");
   projectController
     .get()
-    .then(users => {
+    .then(projects => {
+      console.log("getProjects, projects:", projects);
       res
         .status(200) //success
-        .json(users);
+        .json(projects);
     })
     .catch(e => {
       console.log(e);
       res
         .status(500) //server error
-        .json({ error: "Error in getStuff" });
+        .json({ error: "Error in getProjects" });
+    });
+};
+
+exports.addProject = (req, res, next) => {
+  console.log("addProject: ", req.body);
+  projectController
+    .insert(req.body)
+    .then(project => {
+      console.log("addProject, project: ", project);
+      res
+        .status(200) //success
+        .json(project);
+    })
+    .catch(e => {
+      res
+        .status(500) //server error
+        .json({ error: "error in addProject" });
     });
 };
--- data/routers/projectRouter.jsx
+++ data/routers/projectRouter.jsx
@@ -5,9 +5,29 @@ const projectDB = require("../helpers/projectModel");
 
 console.log("projectRouter.jsx is running....");
 
-const { getStuff } = require("../controllers/projectController.jsx");
+const {
+  getProjects,
+  addProject
+} = require("../controllers/projectController.jsx");
 
-router.route("/").get(getStuff);
-// .post()
+router
+  .route("/")
+  .get(getProjects)
+  .post(validatePost, addProject);
+
+function validatePost(req, res, next) {
+  console.log("validatePost: ", req.body);
+  if (!req.body) {
+    res
+      .status(400) //Bad Request
+      .json({ message: "Missing post information" });
+  } else if (!req.body.name || !req.body.description) {
+    res
+      .status(400) //Bad Request
+      .json({ message: "Missing post name and/or description" });
+  } else {
+    next();
+  }
+}
 
 module.exports = router;
