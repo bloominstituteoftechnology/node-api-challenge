d8d52533a5f29e3c95cb7ce82e1eafb50eb0a0e2
Luis Hernandez <luishrd@gmail.com>
2020-02-13 18:06:50 -0400

    minor refactor

--- data/helpers/projectModel.js
+++ data/helpers/projectModel.js
@@ -2,60 +2,60 @@ const db = require("../dbConfig.js");
 const mappers = require("./mappers");
 
 module.exports = {
-    get,
-    insert,
-    update,
-    remove,
-    getProjectActions,
+  get,
+  insert,
+  update,
+  remove,
+  getProjectActions,
 };
 
 function get(id) {
-    let query = db("projects as p");
+  let query = db("projects as p");
 
-    if (id) {
-        query.where("p.id", id).first();
+  if (id) {
+    query.where("p.id", id).first();
 
-        const promises = [query, this.getProjectActions(id)]; // [ projects, actions ]
+    const promises = [query, getProjectActions(id)]; // [ projects, actions ]
 
-        return Promise.all(promises).then(function(results) {
-            let [project, actions] = results;
+    return Promise.all(promises).then(function(results) {
+      let [project, actions] = results;
 
-            if (project) {
-                project.actions = actions;
+      if (project) {
+        project.actions = actions;
 
-                return mappers.projectToBody(project);
-            } else {
-                return null;
-            }
-        });
-    } else {
-        return query.then(projects => {
-            return projects.map(project => mappers.projectToBody(project));
-        });
-    }
+        return mappers.projectToBody(project);
+      } else {
+        return null;
+      }
+    });
+  } else {
+    return query.then(projects => {
+      return projects.map(project => mappers.projectToBody(project));
+    });
+  }
 }
 
 function insert(project) {
-    return db("projects")
-        .insert(project, "id")
-        .then(([id]) => this.get(id));
+  return db("projects")
+    .insert(project, "id")
+    .then(([id]) => get(id));
 }
 
 function update(id, changes) {
-    return db("projects")
-        .where("id", id)
-        .update(changes)
-        .then(count => (count > 0 ? this.get(id) : null));
+  return db("projects")
+    .where("id", id)
+    .update(changes)
+    .then(count => (count > 0 ? get(id) : null));
 }
 
 function remove(id) {
-    return db("projects")
-        .where("id", id)
-        .del();
+  return db("projects")
+    .where("id", id)
+    .del();
 }
 
 function getProjectActions(projectId) {
-    return db("actions")
-        .where("project_id", projectId)
-        .then(actions => actions.map(action => mappers.actionToBody(action)));
+  return db("actions")
+    .where("project_id", projectId)
+    .then(actions => actions.map(action => mappers.actionToBody(action)));
 }
--- data/seeds/00-cleanup.js
+++ data/seeds/00-cleanup.js
@@ -1,7 +1,8 @@
-const cleaner = require("knex-cleaner");
+const { clean } = require("knex-cleaner");
 
 exports.seed = function(knex) {
-  return cleaner.clean(knex, {
-    ignoreTables: ["knex_migrations", "knex_migrations_lock"]
+  return clean(knex, {
+    mode: "truncate",
+    ignoreTables: ["knex_migrations", "knex_migrations_lock"],
   });
 };
