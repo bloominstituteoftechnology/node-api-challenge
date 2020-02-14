be90365bcd5318dd23bef1aedbef1ee569ccec8f
Sierra Curtis <sierracurtis91@gmail.com>
2020-02-14 12:45:20 -0500

    get, get by id and post is working

--- routers/projectRouter.js
+++ routers/projectRouter.js
@@ -5,7 +5,61 @@ const Projects = require('../data/helpers/projectModel');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-    Projects.get().then().catch()
-})
+    Projects
+    .get()
+    .then(project => {
+        res.status(200).json(project)
+    })
+    .catch(err => {
+        console.log(err)
+        res.status(500).json({ error: 'Could not retrieve projects from the database' })
+    })
+});
+
+router.get('/:id', validateProjectId, (req, res) => {
+    const { id } = req.params;
+    Projects
+    .get(id)
+    .then(project => {
+        res.status(201).json(project)
+    })
+});
+
+router.post('/', validateProject, (req, res) => {
+    Projects
+    .insert(req.body)
+    .then(project => {
+        res.status(200).json(project)
+    })
+    .catch(err => {
+        res.status(500).json({ error: 'error adding user' })
+    })
+});
+
+function validateProjectId(req, res, next) {
+    const { id } = req.params;
+    Projects.get(id)
+    .then(project => {
+      if (project) {
+        req.project = project;
+        next();
+      } else {
+        res.status(400).json({ message: 'Invalid project by ID' })
+      }
+    })
+    .catch(err  => {
+      res.status(500).json({ error: 'The project information could not be retrieved'})
+    })
+  };
+
+function validateProject(req, res, next) {
+    const project = req.body;
+        if (!project) {
+            res.status(400).json({ message: 'missing project data' })
+        } else if (!project.name || !project.description) {
+        res.status(400).json({ message: 'missing required name and description field' })
+    }
+    next();
+};
 
 module.exports = router;
