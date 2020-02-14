7c75f6b0d238f09b2b68c1c2a2ea3120f042c956
wesley.moody <wgm3190@gmail.com>
2020-02-14 12:24:52 -0600

    routes

--- server.js
+++ server.js
@@ -1,9 +1,15 @@
-const express = require('express');
+const express = require("express");
+const actionsRoutes = require("./routes/actionsRoutes");
+const projectsRoutes = require("./routes/projectsRoutes");
 
 const server = express();
 
 server.get("/", (req, res) => {
   res.status(200).json({ message: "Lemme work it." });
 });
+
+//  Routing
+server.use("./actions", actionsRoutes);
+server.use("./projects", projectsRoutes);
 
 module.exports = server;
