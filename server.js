ec142d83d5b27c83784eaebd1c17e71ec278d39a
Sierra Curtis <sierracurtis91@gmail.com>
2020-02-14 11:24:56 -0500

    set up routes for actions and projects

--- server.js
+++ server.js
@@ -1,3 +1,15 @@
 const express = require('express');
 const server = express();
 
+const actionRouter = require('./routers/actionRouter');
+const projectRouter = require('./routers/projectRouter');
+
+server.use(express.json());
+server.use('/actions', actionRouter);
+server.use('/projects', projectRouter);
+
+server.get('/', (req, res) => {
+    res.status(200).json({ message: 'App is working' });
+});
+
+module.exports = server;
