94e6915f544066779ce1be1f5115b03158e54b5c
Sierra Curtis <sierracurtis91@gmail.com>
2020-02-14 11:16:55 -0500

    set up server and installed dotenv

--- index.js
+++ index.js
@@ -12,3 +12,11 @@ I need this code, but don't know where, perhaps should make some middleware, don
 
 Go code!
 */
+require('dotenv').config();
+
+const server = require('./server');
+const port = process.env.PORT || 5000;
+
+server.listen(port, () => {
+    console.log(`\n** API is listening on port ${port} **\n`)
+})
