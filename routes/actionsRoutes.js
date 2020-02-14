a830e0ffb2b64874520e976fcd03b41b81f8f9bb
wesley.moody <wgm3190@gmail.com>
2020-02-14 14:13:40 -0600

    actions DELETE

--- routes/actionsRoutes.js
+++ routes/actionsRoutes.js
@@ -24,62 +24,69 @@ router.get(".:id", (req, res) => {
       res.json(action);
     })
     .catch(() => {
-      res.status(500).json({ message: "Error retrieving action from database." });
+      res
+        .status(500)
+        .json({ message: "Error retrieving action from database." });
     });
 });
 
 // POST =======>
 
-router.post('/', (req, res) => {
-  const action = req.body
+router.post("/", (req, res) => {
+  const action = req.body;
   if (action.project_id && action.description && action.notes) {
     actionsDB
-    .insert(action)
-    .then(() => {
-      res.status(200).json({ message: 'Add new action to database.' })
-    })
-    .catch(() => {
-      res.status(400).json({ message: 'Error occurred when adding new action to database' })
-    })
+      .insert(action)
+      .then(() => {
+        res.status(200).json({ message: "Add new action to database." });
+      })
+      .catch(() => {
+        res
+          .status(400)
+          .json({
+            message: "Error occurred when adding new action to database"
+          });
+      });
   } else {
     res.catch(() => {
-      res.status(500).json({ message: "Unable to add new action to database." })
-    })
+      res
+        .status(500)
+        .json({ message: "Unable to add new action to database." });
+    });
   }
 
   // PUT =======>
 
-  router.put('/:id', (req, res) => {
+  router.put("/:id", (req, res) => {
     const { id } = req.params;
     const newAction = req.body;
-    if(newAction.description, newAction.notes, newAction.project_id) {
-      actionsDB
-      .update(id, newAction)
-      .then(() => {
-        res.status(200).json({ message: 'Action successfully updated' })
-      })
+    if ((newAction.description, newAction.notes, newAction.project_id)) {
+      actionsDB.update(id, newAction).then(() => {
+        res.status(200).json({ message: "Action successfully updated" });
+      });
     } else {
       res.catch(() => {
-        res.status(400)({ message: "Error with request." })
-      })
+        res.status(400)({ message: "Error with request." });
+      });
       res.catch(() => {
-        res.status(500).json({ message: "Error updating action" })
-      })
+        res.status(500).json({ message: "Error updating action" });
+      });
     }
-  })
+  });
 
   // DELETE =======>
 
   router.delete("/:id", (req, res) => {
-    
-  })
-
-
-
-
-
-})
-
-
+    const { id } = req.params;
+    actionsDB.remove(id);
+    then(actions => {
+      res.json(actions);
+    }).catch(() => {
+      res
+        .status(500)
+        .json({ message: "Error removing actions from database." });
+    });
+  });
+});
 
 module.exports = router;
