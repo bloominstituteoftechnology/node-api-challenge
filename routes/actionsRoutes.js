6bf89b8e514643ea97ee144e76595020db008a0d
wesley.moody <wgm3190@gmail.com>
2020-02-14 12:46:12 -0600

    GET requests action and projects both done

--- routes/actionsRoutes.js
+++ routes/actionsRoutes.js
@@ -1,9 +1,31 @@
 const express = require("express");
-
 const router = express.Router();
+const Db = require("../data/helpers/actionModel");
 
 router.get("/", (req, res) => {
-  res.status(200).json({ message: "GET to '/actions' successful" });
+  Db.get()
+    .then(actions => {
+      res.status(200).json(actions);
+    })
+    .catch(err => {
+      res
+        .status(500)
+        .json({ message: "Could not retrieve data from database" });
+    });
+});
+
+router.get(".:id", (req, res) => {
+  const { id } = req.params;
+  actionsDB
+    .get(id)
+    .then(action => {
+      res.json(action);
+    })
+    .catch(() => {
+      res
+        .status(500)
+        .json({ message: "Error retrieving action from database." });
+    });
 });
 
 module.exports = router;
--- routes/projectsRoutes.js
+++ routes/projectsRoutes.js
@@ -1,9 +1,31 @@
-const express = require('express');
-
+const express = require("express");
 const router = express.Router();
+const Db = require("../data/helpers/projectModel");
 
 router.get("/", (req, res) => {
-  res.status(200).json({ message: "GET to '/projects' successful" })
-})
+  Db.get()
+    .then(projects => {
+      res.status(200).json(projects);
+    })
+    .catch(err => {
+      res
+        .status(500)
+        .json({ message: "Could not retrieve data from database" });
+    });
+});
+
+router.get(".:id", (req, res) => {
+  const { id } = req.params;
+  projectsDB
+    .get(id)
+    .then(projects => {
+      res.json(projects);
+    })
+    .catch(() => {
+      res
+        .status(500)
+        .json({ message: "Error retrieving projects from database." });
+    });
+});
 
 module.exports = router;
