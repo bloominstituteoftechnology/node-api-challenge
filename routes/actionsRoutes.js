e4334937a40f53415c1812ce56922e9d17c43d67
wesley.moody <wgm3190@gmail.com>
2020-02-14 13:55:12 -0600

    action POST

--- routes/actionsRoutes.js
+++ routes/actionsRoutes.js
@@ -2,6 +2,8 @@ const express = require("express");
 const router = express.Router();
 const Db = require("../data/helpers/actionModel");
 
+// GET =======>
+
 router.get("/", (req, res) => {
   Db.get()
     .then(actions => {
@@ -22,10 +24,30 @@ router.get(".:id", (req, res) => {
       res.json(action);
     })
     .catch(() => {
-      res
-        .status(500)
-        .json({ message: "Error retrieving action from database." });
+      res.status(500).json({ message: "Error retrieving action from database." });
     });
 });
+
+// POST =======>
+
+router.post('/', (req, res) => {
+  const action = req.body
+  if (action.project_id && action.description && action.notes) {
+    actionsDB
+    .insert(action)
+    .then(() => {
+      res.status(200).json({ message: 'Add new action to database.' })
+    })
+    .catch(() => {
+      res.status(400).json({ message: 'Error occurred when adding new action to database' })
+    })
+  } else {
+    res.catch(() => {
+      res.status(500).json({ message: "Unable to add new action to database." })
+    })
+  }
+})
+
+
 
 module.exports = router;
