75d59bd6c2a770dfb2ca09a66da62741a34ed91f
chiragthesia <harithesia@gmail.com>
2020-02-23 20:55:44 -0600

    MVP MET

--- data/seeds/01-projects.js
+++ data/seeds/01-projects.js
@@ -1,9 +1,9 @@
 exports.seed = function(knex, Promise) {
-  return knex('projects').insert([
-    {
-      name: 'Complete Node.js and Express Challenge',
-      description:
-        'Build and Awesome API Using Node.js and Express to Manage Projects and Actions GTD Style!',
-    },
-  ]);
+	return knex('projects').insert([
+		{
+			id: 1,
+			name: 'Complete Node.js and Express Challenge',
+			description: 'Build and Awesome API Using Node.js and Express to Manage Projects and Actions GTD Style!'
+		}
+	]);
 };
