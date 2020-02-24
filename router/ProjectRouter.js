86e9c28207bf094a46d1a1bd5081a0fef8763291
Romairo1987 <49536760+Romairo1987@users.noreply.github.com>
2020-02-23 22:48:56 -0800

    mvp done

--- router/ProjectRouter.js
+++ router/ProjectRouter.js
@@ -1,77 +1,48 @@
 const express=require("express")
 const router=express.Router()
 const projectdb=require('../data/helpers/projectModel')
-const actiondb=require('../data/helpers/actionModel')
 
-router.get("/:id",validateProjectId, async(req,res)=>{
+// Routing
+
+router.get("/:id",validateProjectId, async(req,res,next)=>{
     try{
         const data=await projectdb.get(req.params.id)
         res.status(200).json(data)
     }catch(err){
-        res.status(500).json({message:"server error."})
+        next(err)
     }
 })
 
-router.post("/",validateproject, async(req,res)=>{
+router.post("/",validateproject, async(req,res,next)=>{
     try{
         const data=await projectdb.insert(req.body)
         res.json(data)
     }catch(err){
-        res.status(500).json({message:err})
+        next(err)
     }
 })
 
-router.put("/:id",validateProjectId,validateproject,async(req,res)=>{
+router.put("/:id",validateProjectId,validateproject,async(req,res,next)=>{
     try{
         const data=await projectdb.update(req.params.id, req.body)
         res.json(data)
     }catch(err){
-        res.status(500).json({message:err})
+        next(err)
     }
 })
 
-router.delete("/:id",validateProjectId, async(req,res)=>{
+router.delete("/:id",validateProjectId, async(req,res,next)=>{
     try{
         await projectdb.remove(req.params.id)
         res.json({message:"successfully deleted."})
     }catch(err){
-        res.status(500).json({message:err})
+        next(err)
     }
     
 })
 
-router.get("/:id/actions",validateProjectId,async(req,res)=>{
-    try{
-        const actions=await projectdb.getProjectActions(req.params.id)
-        if(actions[0]===undefined){
-            res.status(404).json({message:"this project has no actions."})
-        }else{
-            res.json(actions)
-        }   
-    }catch(err){
-        res.status(500).json({message:err})
-    }
-})
 
-router.get('/:id/actions/:actionId',validateProjectId,validateActionId,async(req,res)=>{
-    try{
-        const data=await actiondb.get(req.params.actionId)
-        console.log(data)
-        res.json(data)
-    }catch(err){
-        res.status(500).json({message:"Server Error"})
-    }
-})
-
-router.post('/:id/actions/',validateaction,async(req,res)=>{
-    try{  
-        const data= await actiondb.insert(req.body)
-        console.log(data)
-        res.json(data)
-    }catch(err){
-        res.status(500).json({message:"dfjrhf"})
-    }
-})
+// MiddleWare Functions
 
 
 async function validateProjectId(req,res,next){
@@ -83,6 +54,7 @@ async function validateProjectId(req,res,next){
         }
 }
 
+
 function validateproject(req,res,next){
     const {name,description}=req.body
     if(!name||!description){
@@ -92,27 +64,5 @@ function validateproject(req,res,next){
     }
 }
 
-async function validateActionId(req,res,next){
-    const id=await actiondb.get(req.params.actionId)
-    if(!id){
-        res.status(404).json({message:"this action with this specific id is not found"})
-    }else{
-        next()
-    }
-}
-
-async function validateaction(req,res,next){
-    const id = await projectdb.get(req.params.id)
-    
-    const {description,notes,project_id}=req.body
-    if(!id){
-        res.status(404).json({message:"can't add action to not existing project"})
-    }else if(!description||!notes){
-        res.status(400).json({message:"please provide description and notes "})
-    }else{
-        next()
-    }
-    
-}
 
 module.exports=router
--- server.js
+++ server.js
@@ -1,14 +1,18 @@
 const express =require('express')
 const projectRouter=require('./router/projectRouter')
+const actionRouter=require('./router/ActionRouter')
 const server=express()
 
 server.use(express.json());
 server.use("/project",projectRouter)
-
+server.use("/project",actionRouter)
 
 server.get("/",(req,res)=>{
-    res.send('<h1>Hello<h1/>')
+    res.send('<h1>Wellcome!<h1/>')
 })
 
+server.use((err,req,res,next)=>{
+    res.status(500).json({message:"Server Error"})
+})
 
 module.exports = server;
