14e198599d6944633f659c51da1df47380d6820d
Ricardo Barbosa <ric3639@gmail.com>
2020-02-23 17:23:46 -0800

    server.js added

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const projectRouter = require('./data/projects/projectRouter')
+const actionRouter = require('./data/actions/actionRouter');
+
+const server = express()
+server.use(express.json())
+
+server.use('/api/projects', projectRouter)
+server.use('/api/actions', actionRouter)
+
+server.get('/', (req, res) => {
+   res.send(`<h1>Welcome to api-challenge</h1>`)
+});
+
+function errorHandler(err, req, res, next) {
+   res.status(err.status || 500).json({
+      message: err.message || "Internal Server Error"
+   })
+}
+
+server.use(errorHandler)
+
+module.exports = server;
