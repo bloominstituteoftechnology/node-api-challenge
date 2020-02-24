6ce7c28df6817aea0c8c2ae43dfaa3517254ed15
CLYPR Technologies <cbanks@clypr.co>
2020-02-23 20:55:09 -0500

    server completed and routers added

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const projectRouter = require("./routers/projectRouter");
+const actionRouter = require("./routers/actionRouter");
+
+const server = express();
+server.use(express.json());
+server.use(logger);
+server.use('/api/projects', projectRouter);
+server.use('/api/actions', actionRouter);
+
+server.get('/', (req, res) => {
+  res.send(`Working...`);
+});
+
+function logger(req, res, next) {
+  const { method, originalUrl } = req;
+  console.log(`[${new Date().toISOString()}] : ${method} to ${originalUrl}`);
+  next();
+}
+
+module.exports = server; 
