e33da9976bab814b61109b981e46f8adc620b0a1
Romairo1987 <49536760+Romairo1987@users.noreply.github.com>
2020-02-23 18:56:29 -0800

    working

--- index.js
+++ index.js
@@ -12,3 +12,11 @@ I need this code, but don't know where, perhaps should make some middleware, don
 
 Go code!
 */
+
+const port =2020
+const server=require('./server')
+
+server.listen(port,()=>{
+    console.log(`server listen on port ${port}`)
+})
+
