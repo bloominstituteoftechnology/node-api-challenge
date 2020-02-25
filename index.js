8545def132f578df52ea020c20199ab92430aa2c
chefboyrdeuce <jpldeuce@gmail.com>
2020-02-24 21:35:48 -0500

    Created action routes, project routes, server.js, router.js

--- index.js
+++ index.js
@@ -12,3 +12,11 @@ I need this code, but don't know where, perhaps should make some middleware, don
 
 Go code!
 */
+
+const server = require('./server');
+
+const port = 4000;
+
+server.listen(port, () => {
+	console.log(`Server listening on port: ${port}`);
+});
