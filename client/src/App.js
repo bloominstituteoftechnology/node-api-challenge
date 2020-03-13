b3c5163bf80ed9fee6bf1de2aca77859948c28af
johnkirtley <kirtleyj16@gmail.com>
2020-03-13 10:54:47 -0700

    Added card styling

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,52 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import Styled from 'styled-components';
+import { Route, Link } from 'react-router-dom';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+	const [projects, setProjects] = useState([]);
+
+	useEffect(() => {
+		axios
+			.get('https://backend-sprint-john.herokuapp.com/api/projects/')
+			.then(res => {
+				// console.log(res.data);
+				setProjects(res.data);
+			})
+			.catch(err => {
+				console.log(err);
+			});
+	}, []);
+
+	const Container = Styled.div`
+    display: grid;
+    grid-template-columns: repeat(3, 1fr);
+    justify-items: center;
+    text-align: center;
+  `;
+
+	const Card = Styled.div`
+  border: 1px solid black;
+  padding: 4%;
+  `;
+
+	return (
+		<div>
+			<Route path='/'>
+				<h1>Projects</h1>
+				<Container>
+					{projects.map(project => (
+						<Card key={project.id}>
+							<h2>{project.name}</h2>
+							<p>{project.description}</p>
+							<Link to='/:id/actions'>Actions</Link>
+						</Card>
+					))}
+				</Container>
+			</Route>
+		</div>
+	);
+};
 
 export default App;
