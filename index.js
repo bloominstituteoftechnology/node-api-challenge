f5dbc4288212dcea4cd9c061e6593576151f8335
robertelias <robert.elias@live.com>
2020-05-08 08:13:22 -0700

    updated index.js

--- index.js
+++ index.js
@@ -12,3 +12,9 @@ I need this code, but don't know where, perhaps should make some middleware, don
 
 Go code!
 */
+const server = require('./server.js');
+//make the port dynamic
+const port = process.env.PORT || 5000;
+server.listen(port, () => {
+  console.log(`\n*** Server Running on ${port} ***\n`);
+});
