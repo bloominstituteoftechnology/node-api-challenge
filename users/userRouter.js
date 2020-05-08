b4755c37e9ed8a761be6062e2b67bbaebf289d98
robertelias <robert.elias@live.com>
2020-05-08 08:21:25 -0700

    created validations and Post projects

--- users/userRouter.js
+++ users/userRouter.js
@@ -7,5 +7,74 @@ const Actions = require("../data/helpers/actionModel.js");
 const router = express.Router();
 
 
+/* create a project */
 
+
+router.post('/projects', validate_Projects, (req,res) => {
+    Projects.insert(req.body)
+    .then(newProjects => {
+        res.status(201).json(newProjects)
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json({message:"We ran into an issue saving your post"})
+    })
+})
+
+//****************************************************************************** */
+/* Validate */
+function validate_Projects_Id(req, res, next) {
+    const id = req.params.id
+    Projects.get(id)
+    .then(project => {
+      if(!project){
+        res.status(400).json({message: "Invalid project id"})
+      }else {
+         req.project = project
+        next();
+      }
+    })
+    .catch(err => {
+        console.log(err)
+      res.status(500).json({message: "Error finding that project"})
+    })
+  }
+
+  //validate project
+function validate_Projects(req, res, next) {
+    if(!req.body){
+      res.status(400).json({message : "error missing some post data"})
+    }else if(!req.body.name || !req.body.description){
+      res.status(400).json({message : "Error missing the required field"})
+    }else{
+      next();
+    }
+  }
+
+  function validate_Actions(req, res, next) {
+    if(!req.body){
+      res.status(400).json({message : "Error theis is missing Action data"})
+    }else if(!req.body.notes || !req.body.description || !req.body.project_id){
+      res.status(400).json({message : "Error missing the required field"})
+    }else{
+      next();
+    }
+  }
+
+  function validate_Actions_Id(req, res, next) {
+    const id = req.params.id
+    Actions.get(id)
+    .then(action => {
+      if(!action){
+        res.status(400).json({message: "Error invalid action id"})
+      }else {
+         req.action = action
+        next();
+      }
+    })
+    .catch(err => {
+        console.log(err)
+      res.status(500).json({message: "There was an error finding that action"})
+    })
+  }
 module.exports = router;
