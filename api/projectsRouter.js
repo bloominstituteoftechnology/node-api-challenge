2ac891efa400ed8272d8dfc5d80bd2919908d120
Anthony K <antonyk@users.noreply.github.com>
2020-05-08 12:39:59 -0400

    completed all CRUD for Projects

--- api/projectsRouter.js
+++ api/projectsRouter.js
@@ -13,7 +13,7 @@ module.exports = router;
 // description	string	required.
 // completed	boolean	used to indicate if the project has been completed, not required
 
-router.post('/', validatePost, (req, res, next) => {
+router.post('/', validateData, (req, res, next) => {
   // there should be some type of "formatting" prior to sending object to db!!!
   projects.insert(req.body)
   .then(result => {
@@ -37,16 +37,54 @@ router.get('/', (req, res, next) => {
   })
 })
 
-// router.get('/:id', (req, res, next) => {
-//   projects.get()
+router.get('/:id', validateId, (req, res, next) => {
+  projects.get(req.params.id)
+  .then(result => {
+    res.status(200).json(result);
+  })
+  .catch(err => {
+    console.log({err});
+    next(messages.dbRetrieveError);
+  })
+})
 
-// })
+router.put('/:id', validateId, validateData, (req, res, next) => {
+  projects.update(req.params.id, req.body)
+  .then(result => {
+    res.status(200).json(result);
+  })
+  .catch(err => {
+    next(messages.dbUpdateError);
+  })
+})
 
-function validatePost(req, res, next) {
+router.delete('/:id', validateId, (req, res, next) => {
+  projects.remove(req.params.id)
+  .then(result => {
+    res.status(200).json({message: `Deleted Project with ID: ${req.params.id}`})
+  })
+  .catch(err => {
+    next(messages.dbDeleteError);
+  })
+})
+
+function validateData(req, res, next) {
   console.log(req.body)
   if (!req.body || !req.body.name || !req.body.description) {
     next(messages.incompleteData);
   } else {
     next();
   }
+}
+
+function validateId(req, res, next) {
+  if (!req.params || !req.params.id || !isIntAsString(req.params.id)) {
+    next(messages.notAcceptableValue)
+  } else {
+    next();
+  }
+}
+
+function isIntAsString(value) {
+  return /^\d+$/.test(value);
 }
