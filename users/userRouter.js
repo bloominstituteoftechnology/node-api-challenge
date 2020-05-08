b87c68fbcf865d47ffa67e56a102e7fc15b2239f
robertelias <robert.elias@live.com>
2020-05-08 08:32:20 -0700

    get /projects/:id/actions by id

--- users/userRouter.js
+++ users/userRouter.js
@@ -1,151 +1,189 @@
-const express = require("express");
+const express = require ('express');
 
-const Projects = require("../data/helpers/projectModel.js");
-const Actions = require("../data/helpers/actionModel.js");
+const Projects = require ('../data/helpers/projectModel.js');
+const Actions = require ('../data/helpers/actionModel.js');
 
+const router = express.Router ();
 
-const router = express.Router();
+//***********************************Projects***********************************//
 
-
-/* create a project */
-
-
-router.post('/projects', validate_Projects, (req,res) => {
-    Projects.insert(req.body)
-    .then(newProjects => {
-        res.status(201).json(newProjects)
+//Create a new project */
+//POST
+//@Route api/users/projects
+router.post ('/projects', validate_Projects, (req, res) => {
+  Projects.insert (req.body)
+    .then (newProjects => {
+      res.status (201).json (newProjects);
     })
-    .catch(err => {
-        console.log(err);
-        res.status(500).json({message:"We ran into an issue saving your post"})
+    .catch (err => {
+      console.log (err);
+      res
+        .status (500)
+        .json ({message: 'We ran into an issue saving your post'});
+    });
+});
+
+//listing ALL projects
+//GET
+//@Route users/projects
+router.get ('/projects', (req, res) => {
+  Projects.get ()
+    .then (projects => {
+      res.status (200).json (projects);
     })
-})
+    .catch (err => {
+      console.log (err);
+      res.status (500).json ({message: 'Error fetching projects'});
+    });
+});
 
-/* listing projects  */
-
-
-router.get('/projects', (req,res) => {
-    Projects.get()
-    .then(projects => {
-        res.status(200).json(projects);
+//listing a project by ID
+//GET
+//@Route users/projects/:id
+router.get ('/projects/:id', validate_Projects_Id, (req, res) => {
+  const id = req.params.id;
+  Projects.get (id)
+    .then (projects => {
+      res.status (200).json (projects);
     })
-    .catch(err => {
-        console.log( err);
-        res.status(500).json({message: "Error fetching projects"})
-    })
-})
+    .catch (err => {
+      console.log (err);
+      res
+        .status (500)
+        .json ({message: 'There was an error finding that project'});
+    });
+});
 
-/* get projects */
-router.get('/projects/:id', validate_Projects_Id, (req, res) => {
+//Updates project by ID
+//PUT
+//@Route users/projects/:id
+router.put (
+  '/projects/:id',
+  validate_Projects_Id,
+  validate_Projects,
+  (req, res) => {
+    const id = req.params.id;
+    Projects.update (id, req.body)
+      .then (updatedProjects => {
+        res.status (201).json (updatedProjects);
+      })
+      .catch (err => {
+        console.log (err);
+        res.status (500).json ({message: 'Error updating this project'});
+      });
+  }
+);
+
+//Delete a project by ID
+//DELETE
+//@Route users/projects/:id
+router.delete ('/projects/:id', validate_Projects_Id, (req, res) => {
+  const id = req.params.id;
+  Projects.remove (id)
+    .then (projectToDelete => {
+      console.log ('The project has been deleted');
+      res.status (200).json (req.project);
+    })
+    .catch (err => {
+      console.log (err);
+      res.status (500).json ({message: 'Error deleting project'});
+    });
+});
+
+//********************Actions*************************************************//
+
+//Create a new ACTION
+//PUT
+//@Route users/projects/:id/actions
+router.post (
+  '/projects/:id/actions',
+  validate_Projects_Id,
+  validate_Actions,
+  (req, res) => {
+    Actions.insert (req.body)
+      .then (newActions => {
+        res.status (201).json (newActions);
+      })
+      .catch (err => {
+        console.log (err);
+        res.status (500).json ({message: 'Error saving your action'});
+      });
+  }
+);
+
+//Get a new ACTION by id
+//GET
+//@Route users/projects/:id/actions
+router.get('/projects/:id/actions', validate_Projects_Id, (req,res) => {
     const id = req.params.id
-    Projects.get(id)
-    .then(projects => {
-        res.status(200).json(projects)
+    Projects.getProjectActions(id)
+    .then(projActions => {
+        res.status(200).json(projActions)
     })
     .catch(err => {
-      console.log(err)
-      res.status(500).json({message: "There was an error finding that project"})
-    })
-  });
-
-   /* Updates  */
-   router.put('/projects/:id', validate_Projects_Id, validate_Projects, (req,res) => {
-    const id = req.params.id;
-    Projects.update(id, req.body)
-    .then(updatedProjects => {
-        res.status(201).json(updatedProjects);
-    })
-    .catch(err => {
-        console.log(err);
-        res.status(500).json({message: "Error updating this project"})
+        console.log(err)
+        res.status(500).json({message: "Error getting actions for project"})
     })
 })
 
-  /* Delete */
-  router.delete('/projects/:id',validate_Projects_Id, (req, res) => {
-    const id = req.params.id;
-    Projects.remove(id)
-    .then(projectToDelete => {
-      console.log("The project has been deleted")
-      res.status(200).json(req.project)
-    })
-    .catch(err => {
-      console.log(err)
-      res.status(500).json({message: "Error deleting project"})
-    })
-  });
-
-//*********************************************************************
-  /*     Actions */
-
-  /* New Actions */
-
-  router.post('/projects/:id/actions', validate_Projects_Id, validate_Actions, (req,res) => {
-    Actions.insert(req.body)
-    .then(newActions => {
-        res.status(201).json(newActions)
-    })
-    .catch(err => {
-        console.log(err);
-        res.status(500).json({message:"Error saving your action"})
-    })
-})
 
 
 //****************************************************************************** */
 /* Validate */
-function validate_Projects_Id(req, res, next) {
-    const id = req.params.id
-    Projects.get(id)
-    .then(project => {
-      if(!project){
-        res.status(400).json({message: "Invalid project id"})
-      }else {
-         req.project = project
-        next();
+function validate_Projects_Id (req, res, next) {
+  const id = req.params.id;
+  Projects.get (id)
+    .then (project => {
+      if (!project) {
+        res.status (400).json ({message: 'Invalid project id'});
+      } else {
+        req.project = project;
+        next ();
       }
     })
-    .catch(err => {
-        console.log(err)
-      res.status(500).json({message: "Error finding that project"})
-    })
-  }
+    .catch (err => {
+      console.log (err);
+      res.status (500).json ({message: 'Error finding that project'});
+    });
+}
 
-  //validate project
-function validate_Projects(req, res, next) {
-    if(!req.body){
-      res.status(400).json({message : "error missing some post data"})
-    }else if(!req.body.name || !req.body.description){
-      res.status(400).json({message : "Error missing the required field"})
-    }else{
-      next();
-    }
+//validate project
+function validate_Projects (req, res, next) {
+  if (!req.body) {
+    res.status (400).json ({message: 'error missing some post data'});
+  } else if (!req.body.name || !req.body.description) {
+    res.status (400).json ({message: 'Error missing the required field'});
+  } else {
+    next ();
   }
+}
 
-  function validate_Actions(req, res, next) {
-    if(!req.body){
-      res.status(400).json({message : "Error theis is missing Action data"})
-    }else if(!req.body.notes || !req.body.description || !req.body.project_id){
-      res.status(400).json({message : "Error missing the required field"})
-    }else{
-      next();
-    }
+function validate_Actions (req, res, next) {
+  if (!req.body) {
+    res.status (400).json ({message: 'Error theis is missing Action data'});
+  } else if (!req.body.notes || !req.body.description || !req.body.project_id) {
+    res.status (400).json ({message: 'Error missing the required field'});
+  } else {
+    next ();
   }
+}
 
-  function validate_Actions_Id(req, res, next) {
-    const id = req.params.id
-    Actions.get(id)
-    .then(action => {
-      if(!action){
-        res.status(400).json({message: "Error invalid action id"})
-      }else {
-         req.action = action
-        next();
+function validate_Actions_Id (req, res, next) {
+  const id = req.params.id;
+  Actions.get (id)
+    .then (action => {
+      if (!action) {
+        res.status (400).json ({message: 'Error invalid action id'});
+      } else {
+        req.action = action;
+        next ();
       }
     })
-    .catch(err => {
-        console.log(err)
-      res.status(500).json({message: "There was an error finding that action"})
-    })
-  }
+    .catch (err => {
+      console.log (err);
+      res
+        .status (500)
+        .json ({message: 'There was an error finding that action'});
+    });
+}
 module.exports = router;
