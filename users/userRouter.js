26f3ebd3c71f8a6c17396051312115a1a0a4a40b
robertelias <robert.elias@live.com>
2020-05-08 08:18:19 -0700

    created userRouter.js file

---
+++ users/userRouter.js
@@ -0,0 +1,11 @@
+const express = require("express");
+
+const Projects = require("../data/helpers/projectModel.js");
+const Actions = require("../data/helpers/actionModel.js");
+
+
+const router = express.Router();
+
+
+
+module.exports = router;
