9dda71c79ff5326a935d560ed0e3713636a7733c
Anthony K <antonyk@users.noreply.github.com>
2020-05-08 11:24:13 -0400

    configured express with global mw and routes

--- index.js
+++ index.js
@@ -12,3 +12,41 @@ I need this code, but don't know where, perhaps should make some middleware, don
 
 Go code!
 */
+// Imports
+const dotenv = require('dotenv').config();
+const express = require('express');
+const cors = require('cors')
+const mw = require('./middleware');
+
+// Basic init and config
+const server = express();
+const port = process.env.PORT || 5000;
+
+// Add global middleware
+server.use(express.json());
+server.use(cors());
+
+
+server.use(mw.logger);
+server.use(mw.errorCatcher);
+// Add routes middleware 
+const api = express.Router();
+server.use('/api/v1', api);
+
+
+api.get('/', (req, res) => {
+  console.log("\n -- root request -- \n")
+  res.send('There is nothing here');
+})
+
+
+
+server.listen(port, () => {
+  console.log(`\n== express on: ${port} ==`)
+})
+
+
+// Helpers and custom middleware
+
+
+
