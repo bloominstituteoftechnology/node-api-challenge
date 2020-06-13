de8f7d47e93ce196034358c50d000ff418f0451c
Andrew Haberman <a.haberman25@gmail.com>
2020-06-12 20:06:30 -0400

    added projects to front end app

--- app/src/App.js
+++ app/src/App.js
@@ -1,23 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import './App.css';
 
 function App() {
+  const [data, setData] = useState([])
+
+  useEffect(() => {
+    axios.get('http://localhost:4000/api/projects')    
+      .then(res => {
+        console.log(res)
+        setData(res.data.project)
+      })
+      .catch(err => {
+        console.log(err)
+      })
+  }, [])
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>My Projects</h1>
+        {data.map((data) => (
+          <p key={data.id}>name: {data.name}</p>
+        ))}
       </header>
     </div>
   );
