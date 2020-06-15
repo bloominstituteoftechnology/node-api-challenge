7db0c1190323c6f86bdb172d6400a610d6e51dff
Andre Freeman <asf@mail.usf.edu>
2020-06-14 23:15:17 -0400

    debugging

--- projects-post.js
+++ projects-post.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const projects = require("./data/helpers/projectModel")
+const actions = require("./data/helpers/actionModel")
 const projectRouter = express.Router();
 
 //GET ENDPOINTS
@@ -19,6 +20,28 @@ projectRouter.get("/projects/:id", (req, res, next) => {
       .catch(next)
   });
 
+  projectRouter.get("/projects/:id/actions/:actionID", checkProjectID(), (req, res, next) => {
+    projects.getProjectActions(req.params.id)
+		.then((projects) => {
+			if (projects) {
+                actions.get(req.params.actionID)
+                .then((action) => {
+                    if (action) {
+                        res.json(action)
+                    } else {
+                        res.status(404).json({
+                            message: "action was not found",
+                        })}
+                })
+			} else {
+				res.status(404).json({
+					message: "Project was not found",
+				})
+			}
+		})
+		.catch(next)
+})
+
   //POST ENDPOINTS
 
   projectRouter.post("/projects", (req, res, next) => {
@@ -35,6 +58,22 @@ projectRouter.get("/projects/:id", (req, res, next) => {
       })
       .catch(next)
   });
+
+  projectRouter.post("/projects/:id/actions", checkProjectID(), (req, res, next) => {
+	if (!req.body.description || !req.body.notes) {
+		return res.status(400).json({
+			message: "Missing description or notes",
+		})
+	}
+
+    actions
+        .insert({...req.body, project_id: req.params.id})
+		.then((action) => {
+			res.status(201).json(action)
+		})
+        .catch(next)
+}) 
+
 
   //PUT ENDPOINT
 
@@ -59,6 +98,8 @@ projectRouter.get("/projects/:id", (req, res, next) => {
       .catch(next)
   });
 
+  
+
   // DELETE ENDPOINTS
 
   projectRouter.delete("/projects/:id", (req, res, next) => {
@@ -77,5 +118,24 @@ projectRouter.get("/projects/:id", (req, res, next) => {
       })
       .catch(next)
   });
+
+  //MIDDLEWARE
+
+  function checkProjectID() {
+	return (req, res, next) => {
+		projects.get(req.params.id)
+			.then((project) => {
+				if (project) {
+					req.project = project
+					next()
+				} else {
+					res.status(404).json({
+						message: "Project not found",
+					})
+				}
+			})
+			.catch(next)
+	}
+}
 
   module.exports = projectRouter;
