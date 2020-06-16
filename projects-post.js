a597c3ebca1c46e2f08257dc2361df065119de68
Andre Freeman <asf@mail.usf.edu>
2020-06-15 20:15:32 -0400

    Added styling

--- projects-post.js
+++ projects-post.js
@@ -1,168 +1,187 @@
 const express = require("express");
-const projects = require("./data/helpers/projectModel")
-const actions = require("./data/helpers/actionModel")
+const projects = require("./data/helpers/projectModel");
+const actions = require("./data/helpers/actionModel");
 const projectRouter = express.Router();
 
 //GET ENDPOINTS
 
+//Get Project by ID
+
 projectRouter.get("/projects/:id", (req, res, next) => {
-    projects
-      .get(req.params.id)
-      .then(project => {
-        if (project) {
-          res.status(200).json(project);
-        } else {
-          res.status(404).json({
-            message: "The project with the specified ID does not exist."
-          });
-        }
-      })
-      .catch(next)
-  });
+  projects
+    .get(req.params.id)
+    .then(project => {
+      if (project) {
+        res.status(200).json(project);
+      } else {
+        res.status(404).json({
+          message: "The project with the specified ID does not exist."
+        });
+      }
+    })
+    .catch(next);
+});
 
-  projectRouter.get("/projects/:id/actions/:actionsID", (req, res, next) => {
+//Get action by ID
+
+projectRouter.get("/projects/:id/actions/:actionsID", (req, res, next) => {
+  actions
+    .get(req.params.actionsID)
+    .then(action => {
+      if (action) {
+        res.status(200).json(action);
+      } else {
+        res.status(404).json({
+          message: "The project with the specified ID does not exist."
+        });
+      }
+    })
+    .catch(next);
+});
+
+//Get all project actions
+
+projectRouter.get("/projects/:id/actions", (req, res, next) => {
+  actions
+    .get()
+    .then(action => {
+      if (action) {
+        res.status(200).json(action);
+      } else {
+        res.status(404).json({
+          message: "The project with the specified ID does not exist."
+        });
+      }
+    })
+    .catch(next);
+});
+
+//POST ENDPOINTS
+
+//Add new project
+
+projectRouter.post("/projects", (req, res, next) => {
+  if (!req.body.name || !req.body.description) {
+    return res.status(400).json({
+      errorMessage: "Please provide name and description for the project."
+    });
+  }
+
+  projects
+    .insert(req.body)
+    .then(project => {
+      res.status(201).json(project);
+    })
+    .catch(next);
+});
+
+//Add new action
+
+projectRouter.post("/projects/:id/actions", (req, res, next) => {
+  if (!req.params.id) {
+    res.status(404).json({
+      message: "The project with the specified ID does not exist."
+    });
+  } else if (!req.body.description || !req.body.notes) {
+    res.status(400).json({
+      errorMessage: "Please provide description and notes for the action."
+    });
+  } else {
     actions
-      .get(req.params.actionsID)
+      .insert({ ...req.body, project_id: req.params.id })
       .then(action => {
-        if (action) {
-          res.status(200).json(action);
-        } else {
-          res.status(404).json({
-            message: "The project with the specified ID does not exist."
-          });
-        }
+        res.status(201).json(action);
       })
-      .catch(next)
-  });
+      .catch(next);
+  }
+});
 
+//PUT ENDPOINT
 
+//Update Project by ID
 
-  //POST ENDPOINTS
+projectRouter.put("/projects/:id", (req, res, next) => {
+  if (!req.body.name || !req.body.description) {
+    return res.status(400).json({
+      errorMessage: "Please provide name and description for the project."
+    });
+  }
 
-  projectRouter.post("/projects", (req, res, next) => {
-    if (!req.body.name || !req.body.description) {
-      return res.status(400).json({
-        errorMessage: "Please provide name and description for the project."
-      });
-    }
-  
-    projects
-      .insert(req.body)
-      .then(project => {
-        res.status(201).json(project);
-      })
-      .catch(next)
-  });
-
-
-  projectRouter.post("/projects/:id/actions", (req, res, next) => {
-    if (!req.params.id) {
-      res.status(404).json({
-        message: "The project with the specified ID does not exist."
-      });
-    } 
-      else if (!req.body.description || !req.body.notes) {
-        res.status(400).json({
-          errorMessage: "Please provide description and notes for the action."
-        });
+  projects
+    .update(req.params.id, req.body)
+    .then(user => {
+      if (user) {
+        res.status(200).json(user);
       } else {
-        actions
-          .insert({ ...req.body, project_id: req.params.id })
-          .then(action => {
-            res.status(201).json(action);
-          })
-          .catch(next);
-    }
-  });
-
-
-
-
-  //PUT ENDPOINT
-
-  projectRouter.put("/projects/:id", (req, res, next) => {
-    if (!req.body.name || !req.body.description) {
-      return res.status(400).json({
-        errorMessage: "Please provide name and description for the project."
-      });
-    }
-  
-    projects
-      .update(req.params.id, req.body)
-      .then(user => {
-        if (user) {
-          res.status(200).json(user);
-        } else {
-          res.status(404).json({
-            message: "The project with the specified ID does not exist."
-          });
-        }
-      })
-      .catch(next)
-  });
-
-
-  projectRouter.put("/projects/:id/actions/:actionsID", (req, res, next) => {
-    if (!req.params.actionsID) {
-      res.status(404).json({
-        message: "The action with the specified ID does not exist."
-      });
-    } 
-      else if (!req.body.description || !req.body.notes) {
-        res.status(400).json({
-          errorMessage: "Please provide description and notes for the action."
+        res.status(404).json({
+          message: "The project with the specified ID does not exist."
         });
-      } else {
-        actions
-          .update(req.params.actionsID, req.body)
-          .then(action => {
-            res.status(201).json(action);
-          })
-          .catch(next);
-    }
-  });
+      }
+    })
+    .catch(next);
+});
 
+//Update action by ID
 
-
-
-  // DELETE ENDPOINTS
-
-  projectRouter.delete("/projects/:id", (req, res, next) => {
-    projects
-      .remove(req.params.id)
-      .then(count => {
-        if (count > 0) {
-          res.status(200).json({
-            message: "The project has been nuked"
-          });
-        } else {
-          res.status(404).json({
-            message: "The project with the specified ID does not exist."
-          });
-        }
-      })
-      .catch(next)
-  });
-
-  projectRouter.delete("/projects/:id/actions/:actionsID", (req, res, next) => {
+projectRouter.put("/projects/:id/actions/:actionsID", (req, res, next) => {
+  if (!req.params.actionsID) {
+    res.status(404).json({
+      message: "The action with the specified ID does not exist."
+    });
+  } else if (!req.body.description || !req.body.notes) {
+    res.status(400).json({
+      errorMessage: "Please provide description and notes for the action."
+    });
+  } else {
     actions
-      .remove(req.params.actionsID)
-      .then(count => {
-        if (count > 0) {
-          res.status(200).json({
-            message: "The action has been nuked"
-          });
-        } else {
-          res.status(404).json({
-            message: "The action with the specified ID does not exist."
-          });
-        }
+      .update(req.params.actionsID, req.body)
+      .then(action => {
+        res.status(201).json(action);
       })
-      .catch(next)
-  });
+      .catch(next);
+  }
+});
 
-  //MIDDLEWARE
+// DELETE ENDPOINTS
 
+//Delete project by ID
 
+projectRouter.delete("/projects/:id", (req, res, next) => {
+  projects
+    .remove(req.params.id)
+    .then(count => {
+      if (count > 0) {
+        res.status(200).json({
+          message: "The project has been nuked"
+        });
+      } else {
+        res.status(404).json({
+          message: "The project with the specified ID does not exist."
+        });
+      }
+    })
+    .catch(next);
+});
 
-  module.exports = projectRouter;
+//Delete action by ID
+
+projectRouter.delete("/projects/:id/actions/:actionsID", (req, res, next) => {
+  actions
+    .remove(req.params.actionsID)
+    .then(count => {
+      if (count > 0) {
+        res.status(200).json({
+          message: "The action has been nuked"
+        });
+      } else {
+        res.status(404).json({
+          message: "The action with the specified ID does not exist."
+        });
+      }
+    })
+    .catch(next);
+});
+
+//MIDDLEWARE
+
+module.exports = projectRouter;
--- server.js
+++ server.js
@@ -1,19 +1,19 @@
-const express = require('express');
-const projectRouter = require("./projects-post")
+const express = require("express");
+const projectRouter = require("./projects-post");
 const server = express();
 
-const port = 4000
+const port = 4000;
 
-server.use(express.json())
-server.use(projectRouter)
+server.use(express.json());
+server.use(projectRouter);
 
 server.use((err, req, res, next) => {
-	console.log(err)
-		res.status(500).json({
-			message: "Something went wrong"
-		})
-	})
+  console.log(err);
+  res.status(500).json({
+    message: "Something went wrong"
+  });
+});
 
 server.listen(port, () => {
-	console.log(`Server running at http://localhost:${port}`)
-})
+  console.log(`Server running at http://localhost:${port}`);
+});
