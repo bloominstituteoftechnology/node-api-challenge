a587d0a21ccc3934d03e74eafeb36f4d49545589
Andre Freeman <asf@mail.usf.edu>
2020-06-15 20:05:36 -0400

    Created Post and Put Actions

--- projects-post.js
+++ projects-post.js
@@ -20,27 +20,22 @@ projectRouter.get("/projects/:id", (req, res, next) => {
       .catch(next)
   });
 
-  projectRouter.get("/projects/:id/actions/:actionID", checkProjectID(), (req, res, next) => {
-    projects.getProjectActions(req.params.id)
-		.then((projects) => {
-			if (projects) {
-                actions.get(req.params.actionID)
-                .then((action) => {
-                    if (action) {
-                        res.json(action)
-                    } else {
-                        res.status(404).json({
-                            message: "action was not found",
-                        })}
-                })
-			} else {
-				res.status(404).json({
-					message: "Project was not found",
-				})
-			}
-		})
-		.catch(next)
-})
+  projectRouter.get("/projects/:id/actions/:actionsID", (req, res, next) => {
+    actions
+      .get(req.params.actionsID)
+      .then(action => {
+        if (action) {
+          res.status(200).json(action);
+        } else {
+          res.status(404).json({
+            message: "The project with the specified ID does not exist."
+          });
+        }
+      })
+      .catch(next)
+  });
+
+
 
   //POST ENDPOINTS
 
@@ -59,20 +54,28 @@ projectRouter.get("/projects/:id", (req, res, next) => {
       .catch(next)
   });
 
-  projectRouter.post("/projects/:id/actions", checkProjectID(), (req, res, next) => {
-	if (!req.body.description || !req.body.notes) {
-		return res.status(400).json({
-			message: "Missing description or notes",
-		})
-	}
 
-    actions
-        .insert({...req.body, project_id: req.params.id})
-		.then((action) => {
-			res.status(201).json(action)
-		})
-        .catch(next)
-}) 
+  projectRouter.post("/projects/:id/actions", (req, res, next) => {
+    if (!req.params.id) {
+      res.status(404).json({
+        message: "The project with the specified ID does not exist."
+      });
+    } 
+      else if (!req.body.description || !req.body.notes) {
+        res.status(400).json({
+          errorMessage: "Please provide description and notes for the action."
+        });
+      } else {
+        actions
+          .insert({ ...req.body, project_id: req.params.id })
+          .then(action => {
+            res.status(201).json(action);
+          })
+          .catch(next);
+    }
+  });
+
+
 
 
   //PUT ENDPOINT
@@ -98,7 +101,29 @@ projectRouter.get("/projects/:id", (req, res, next) => {
       .catch(next)
   });
 
-  
+
+  projectRouter.put("/projects/:id/actions/:actionsID", (req, res, next) => {
+    if (!req.params.actionsID) {
+      res.status(404).json({
+        message: "The action with the specified ID does not exist."
+      });
+    } 
+      else if (!req.body.description || !req.body.notes) {
+        res.status(400).json({
+          errorMessage: "Please provide description and notes for the action."
+        });
+      } else {
+        actions
+          .update(req.params.actionsID, req.body)
+          .then(action => {
+            res.status(201).json(action);
+          })
+          .catch(next);
+    }
+  });
+
+
+
 
   // DELETE ENDPOINTS
 
@@ -121,21 +146,6 @@ projectRouter.get("/projects/:id", (req, res, next) => {
 
   //MIDDLEWARE
 
-  function checkProjectID() {
-	return (req, res, next) => {
-		projects.get(req.params.id)
-			.then((project) => {
-				if (project) {
-					req.project = project
-					next()
-				} else {
-					res.status(404).json({
-						message: "Project not found",
-					})
-				}
-			})
-			.catch(next)
-	}
-}
+
 
   module.exports = projectRouter;
