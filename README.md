4025133708b31cbe4cd6afe426fda6cb76504eba
ferror18 <ferror18@gmail.com>
2020-07-10 09:41:59 -0700

    mvp

--- README.md
+++ README.md
@@ -39,14 +39,25 @@ Commit your code regularly and use descriptive messages. This helps both you (in
 Demonstrate your understanding of this Sprint's concepts by answering the following free-form questions. Edit this document to include your answers after each question. Make sure to leave a blank line above and below your answer so it is clear and easy to read by your Team Lead.
 
 - [ ] Mention two parts of Express that you learned about this week.
+        Routing layer - Layer that connects with front end(endpoints)
+        Bussiness Logic - Bussines specific logic/requirements implemented by company
+        Data layer - Communication with persistente data
 
 - [ ] Describe Middleware?
 
+        middleware is software that provides capabilities that go beyond what the local env can provide
+
 - [ ] Describe a Resource?
+
+        Any information that we might need to store and/or distribute.
 
 - [ ] What can the API return to help clients know if a request was successful?
 
+    Status Code
+
 - [ ] How can we partition our application into sub-applications?
+
+        Diviging it into services, that are not entangled with the frame work.
 
 ## Minimum Viable Product
 
--- index.js
+++ index.js
@@ -12,3 +12,121 @@ I need this code, but don't know where, perhaps should make some middleware, don
 
 Go code!
 */
+
+const express = require('express');
+const server = express();
+const Projects = require('./data/helpers/projectModel.js');
+const Actions = require('./data/helpers/actionModel.js')
+server.use(express.json())
+
+// <<<=== POST ===>>>
+server.post('/api/projects', (req, res) => {
+    Projects.insert(req.body)
+    .then(response => {
+        res.status(201).json(response)
+    })
+})
+
+server.get('/api/projects', (req, res) => {
+    Projects.get()
+    .then(response => {
+        res.status(200).json(response)
+    })
+    .catch(error => {
+        res.status(500).json(error)
+    })
+})
+
+server.put('/api/projects/:id', (req, res) => {
+    Projects.update(req.params.id, req.body)
+    .then(response => {
+        if (response === null) {
+            res.status(404).json({message: "User id is not in Data Base"})
+        } else {
+            res.status(200).json(response)
+        }
+        
+    })
+    .catch(error => {
+        console.log(error);
+        
+        if (error === null) {
+            res.status(404).json({message: "User id is not in Data Base"})
+        } else {
+            res.status(500).json(error)
+        }
+    })
+})
+
+server.delete('/api/projects/:id', (req, res) => {
+    Projects.remove(req.params.id)
+    .then(response => {
+        res.status(200).json(response===1?'User Deleted':'Not an existing User ID')
+    })
+})
+
+server.get('/api/projects/:id/actions', (req, res) => {
+    Projects.get(req.params.id)
+    .then(response => {
+        res.status(200).json(response.actions)
+    })
+})
+// <<<=== Actions ===>>>
+
+server.post('/api/actions', (req, res) => {
+    Actions.insert(req.body)
+    .then(response => {res.status(201).json(response)})
+    .catch(error => {
+        if (error.errno === 19) {
+            res.status(404).json('Project Id not in Data Base')
+        } if (error.errno === 1) {
+            res.status(400).json('Missing a required field')
+        } else {
+            res.status(500).json('Unkown error')
+        }
+    })
+})
+
+server.get('/api/actions', (req, res) => {
+    Actions.get()
+    .then(response => {
+        res.status(200).json(response)
+    })
+    .catch(error => {
+        res.status(500).json(error)
+    })
+})
+
+server.put('/api/actions/:id', (req, res) => {
+    Actions.update(req.params.id, req.body)
+    .then(response => {
+        if (response === null) {
+            res.status(404).json({message: "User id is not in Data Base"})
+        } else {
+            res.status(200).json(response)
+        }
+        
+    })
+    .catch(error => {
+        console.log(error);
+        
+        if (error === null) {
+            res.status(404).json({message: "User id is not in Data Base"})
+        } else {
+            res.status(500).json(error)
+        }
+    })
+})
+
+server.delete('/api/actions/:id', (req, res) => {
+    Actions.remove(req.params.id)
+    .then(response => {
+        res.status(200).json(response===1?'Action Deleted':'That Action is not in Data Base')
+    })
+})
+
+const port = process.env.PORT || 5000;
+server.listen(port, () => {
+  console.log(`\n*** Server Running on http://localhost:${port} ***\n`);
+});
+//hello heroku
