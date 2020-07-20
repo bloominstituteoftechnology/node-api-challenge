b3ef3b7e6cc54e098e2d4e129d6b8b1184641980
jasonbringle <jasonbringle@gmail.com>
2020-07-19 19:05:26 -0700

    finished writing routes for actionRouter...need to test.

--- data/helpers/actionRouter.js
+++ data/helpers/actionRouter.js
@@ -2,9 +2,10 @@ const express = require('express');
 
 const ActionDb = require('../helpers/actionModel');
 const ProjectDb = require('../helpers/projectModel');
+
 const router = express.Router();
 
-router.get("/", (req,res)=>{
+router.get('/', (req,res)=>{
     ActionDb.get()
         .then(actions =>
             res.status(201).json(actions))
@@ -12,6 +13,32 @@ router.get("/", (req,res)=>{
             res.status(500).json({ errormessage: 'Could not get all the actions'}))
 })
 
+router.post('/',(req, res) => {
+    const body = req.body
+    ActionDb.insert(body)
+      .then(action =>
+          res.status(200).json(action))
+      .catch(err =>
+        res.status(500).json({errormessage: 'Could not create the action.'}))
+});
+
+router.put('/:id', (req,res) => {
+    const { id } = req.params;
+    ActionDb.insert(id)
+        .then(action =>
+            res.status(200).json(action))
+        .catch(err =>
+            res.status(500).json({errormessage: "Cannot update action"}))
+})
+
+router.delete("/:id", ( req,res)=>{
+    const { id } = req.params;
+    ActionDb.remove(id)
+      .then(num =>
+        res.status(200).json({ message:`${num} action was deleted`}))
+      .catch(err =>
+        res.status(500).json({ errormessage: "This item was not deleted"}))
+})
 
 
 module.exports = router;
