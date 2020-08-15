1e65f44b5ecdee194fefad18d7c553b0994b35ae
Michael Sheets <michaelsheets@MacBook-Pro.local>
2020-08-14 19:59:32 -0500

    ProjectRouter is finished next up ActionRouter

--- actions/actionRouter.js
+++ actions/actionRouter.js
@@ -1,2 +1,23 @@
 const express = require("express");
 const Actions = require("../data/helpers/actionModel");
+
+const router = express.Router();
+
+router.use("/:id", idExists);
+
+function idExists(req, res, next) {
+  const id = req.params.id;
+  console.log("idd", id);
+
+  Projects.get(id).then((project) => {
+    console.log("project");
+    if (project) {
+      console.log(req.body);
+      console.log("project", project);
+      next();
+    } else {
+      res.status(400).json({ message: "invalid project id" });
+    }
+  });
+}
+module.exports = router;
--- projects/projectRouter.js
+++ projects/projectRouter.js
@@ -1,2 +1,88 @@
 const express = require("express");
 const Projects = require("../data/helpers/projectModel");
+
+const router = express.Router();
+
+//Get Request
+router.get("/", (req, res, next) => {
+  Projects.get(req.params.id)
+    .then((user) => {
+      res.status(200).json(user);
+    })
+    .catch(next);
+});
+
+//Get Request By ID
+router.get("/:id", (req, res, next) => {
+  Projects.get(req.params.id)
+    .then((user) => {
+      res.status(200).json(user);
+    })
+    .catch(next);
+});
+
+//Post request
+
+router.post("/:id", (req, res) => {
+  if (!req.body.name || !req.body.description) {
+    return res.status(400).json({
+      message: "Please Provide infromation ",
+    });
+  }
+  Projects.insert({
+    name: req.body.name,
+    description: req.body.description,
+  })
+    .then((newData) => {
+      res.status(201).json(newData);
+    })
+    .catch((error) => {
+      res.status(500).json({
+        message:
+          "There was some error in insert method while saving the data. ",
+      });
+    });
+});
+
+//Update Request
+router.put("/:id", (req, res) => {
+  const { id } = req.params;
+
+  const changes = req.body;
+  console.log("changes", changes);
+
+  Projects.update(id, changes)
+    .then((updated) => {
+      console.log("updated", updated);
+      res.status(200).json(updated);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json({
+        message: "Error updating the project",
+      });
+    });
+});
+
+//remove
+router.delete("/:id", (req, res) => {
+  Projects.remove(req.params.id)
+    .then((count) => {
+      if (count > 0) {
+        res.status(200).json({
+          message: "The user has been deleted",
+        });
+      } else {
+        res.status(404).json({
+          message: "The post with the specified ID does not exist.",
+        });
+      }
+    })
+    .catch((error) => {
+      res.status(500).json({
+        error: "The post could not be removed",
+      });
+    });
+});
+
+module.exports = router;
