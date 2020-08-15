1622b82f172f75fc965ef27ded9d91f018e34dcb
Chris Forrest <redrider_cr@yahoo.com>
2020-08-14 20:44:06 -0400

    actions finished

--- routers/actionRouter.js
+++ routers/actionRouter.js
@@ -14,15 +14,32 @@ router.get("/", async (req,res) => {
     }
 });
 
+router.get("/:id", validateActionId, async (req,res) => {
+    res.status(200).json(req.action)
+});
+
+router.delete("/:id", validateActionId, async (req,res) =>{
+    try{
+        const { id } = req.params;
+
+        const deletedAction = await Actions.remove(id);
+        res.status(200).json(deletedAction);
+    }catch(err){
+        res.status(500).json({ message: " Couldn't delete action."})
+    }
+})
+
 router.put("/:id",validateActionId, async (req,res) => {
     const { id } = req.params;
     const { description, notes } = req.body;
     const action = await Actions.getById(id)
 
     if(!description || !notes){
-        return res.status(400).json({ message: ""})
+        return res.status(400).json({ message: "Must provide description and notes for the action."})
     }
-} )
+    const newAction = action.update(id, req.body);
+    res.status(200).json(newAction)
+});
 
 async function validateActionId(req,res, next){
     const action = await Actions.get(req.params.id);
